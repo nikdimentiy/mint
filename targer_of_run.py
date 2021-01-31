55143b54b534bcd75bf5370a7096026bf2016ee5
nikdimentiy <nikdimentiy@yahoo.com>
2021-01-31 00:35:36 -0800

    added a list of daily workout

--- targer_of_run.py
+++ targer_of_run.py
@@ -6,15 +6,23 @@
     3. Running as you CAN
     4. Write down the result
     5. The more you run, the better your results
-    6. Create the goal to run N miles (put a bigger goal more interesting will achieve it!)
+    6. Create the goal to run N miles
     7. Increasing the load by 10% every day
     8. Try, do it, YOU CAN!
     9. BE PROUD OF YOURSELF!"""
-x = int(input("Enter the starting distance of the race - (exception 'ZERO'): "))
+x = int(input("Enter the starting distance of the race: "))
 y = int(input("Enter the desired goal: "))
 i = x
+day_workout_goal = []
 day = 1
 while i < y:
     i = i + i * 0.1
+    i = round(i, 2)
+    day_workout_goal.append(i)
     day = day + 1
-print("The goal will be achieved through: ", day)
+cast_day_workout_goal = str(day_workout_goal)
+
+print("The goal will be achieved through: ", day - 1)
+print()
+print("The list of goals to daily running workout: ")
+print(cast_day_workout_goal)
