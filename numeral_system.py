34675af5ada8377ed592278bae9c3242f05f6a2d
nikdimentiy <nikdimentiy@yahoo.com>
2021-02-03 16:55:03 -0800

    Numeral (number system) tiny code

---
+++ numeral_system.py
@@ -0,0 +1,7 @@
+# The bin, oct, and hex functions return strings representing
+# the given number in the corresponding number systems
+
+number = int(input('Please enter an integer number: : '))
+print('Binary system:     ', bin(number))
+print('Octal system:      ', oct(number))
+print('Hexadecimal system:', hex(number))
