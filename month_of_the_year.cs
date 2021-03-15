976f6ca80ae4d8511dfb87b9df219df6e6149ddc
nikdimentiy <nikdimentiy@yahoo.com>
2021-03-14 23:16:39 -0700

    the switch case code in month of the year

---
+++ month_of_the_year.cs
@@ -0,0 +1,25 @@
+
+using System;
+
+namespace Softcode
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite month (number): ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            a = a / 3;
+            switch (a)
+            {
+                case (0): Console.WriteLine("Winter"); break;
+                case (4): Console.WriteLine("Winter"); break;
+                case (1): Console.WriteLine("Spring"); break;
+                case (2): Console.WriteLine("Summer"); break;
+                case (3):
+                    Console.WriteLine("Fall"); break;
+            }
+        }
+    }
+}
+
