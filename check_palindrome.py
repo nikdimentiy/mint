f52a2e2c429517a62fd8fe6704357ae2543981e4
nikdimentiy <nikdimentiy@yahoo.com>
2021-03-24 23:09:41 -0700

    Update check_palindrome.py

--- check_palindrome.py
+++ check_palindrome.py
@@ -1,9 +1,10 @@
 # the tiny code: analyzes whether the entered value is a palindrome
 s = input()
+# let's compare the elements of the sequence in pairs: the first with the last, the second with the penultimate...
 i = 0
-j = len(s) - 1
+j = len(s) - 1 # calculate the length of the sequence and take the last element
 is_polindrom = True
-while i < j:
+while i < j: # step by step compare the elements of the sequence by direction to the conditional middle
     if s[i] != s[j]:
         is_polindrom = False
     i += 1
