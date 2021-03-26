79cc24066f7ec76e5c02d2905c31880acf992674
nikdimentiy <nikdimentiy@yahoo.com>
2021-03-25 23:41:11 -0700

    added new dictionary

--- list_in_dictionary.py
+++ list_in_dictionary.py
@@ -9,3 +9,18 @@ print("You ordered a " + pizza['crust'] + "-crust pizza " +
       "with the following toppings: ")
 for topping in pizza['toppings']:
     print("\t" + topping)
+
+#-----------------------------------------------------------#
+
+favorite_lang = {
+    'jen': ['ruby', 'haskel'],
+    'sarah': ['kotlin', 'c'],
+    'derek': ['python', 'javascript'],
+    'edward': ['java', 'python'],
+    'phil': ['swift', 'scala'],
+}
+for name, languages in favorite_lang.items():
+    print("\n" + name.title() + " favorite language are: ")
+    for language in languages:
+        print("\t" + language.title())
+
