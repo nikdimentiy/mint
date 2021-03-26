8f7d96fe263e3de447a59cae965fb5328be244e8
nikdimentiy <nikdimentiy@yahoo.com>
2021-03-25 23:32:21 -0700

    the code represent: the list in dictionary

---
+++ list_in_dictionary.py
@@ -0,0 +1,11 @@
+# saving information for ordered pizza
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+}
+
+# order description
+print("You ordered a " + pizza['crust'] + "-crust pizza " +
+      "with the following toppings: ")
+for topping in pizza['toppings']:
+    print("\t" + topping)
