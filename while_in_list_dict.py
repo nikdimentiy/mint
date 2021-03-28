b77c7390215413fc1fe95ad4f305a59616d8add2
nikdimentiy <nikdimentiy@yahoo.com>
2021-03-27 22:27:55 -0700

    cycle while in list and dictionary

--- while_in_list_dict.py
+++ while_in_list_dict.py
@@ -20,3 +20,21 @@ while 'cat' in pets:
 
 print(len(pets))
 print(pets)
+
+responses = {}
+
+polling_active = True
+
+while polling_active:
+    name = input("\nWhat is your name: ")
+    response = input("Which mountain would you like to climb someday? ")
+
+    responses[name] = response
+    repeat = input("Would you like to let anther person respond? (yes / no) ")
+    if repeat == 'no':
+        polling_active = False
+
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+    print(name.title() + " would like to climb " + response + "!")
+
