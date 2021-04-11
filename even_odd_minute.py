c5853b740d6480b06096b4ab1c1a22c1b287cd65
nikdimentiy <nikdimentiy@yahoo.com>
2021-04-10 22:56:43 -0700

    the code represent array minutes of the hour and show how minute is now - even or odd

---
+++ even_odd_minute.py
@@ -0,0 +1,11 @@
+#  the code represent array minutes of the hour and show how minute is now - even or odd
+from datetime import datetime
+
+odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57,
+        59]
+right_this_minute = datetime.today().minute
+
+if right_this_minute in odds:
+    print("It's an ODD minute!")
+else:
+    print("It's an EVEN minute!!")
