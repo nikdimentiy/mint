3375e8b80df3d5b1d2fed2cf687c55cba4d8caa2
nikdimentiy <nikdimentiy@yahoo.com>
2021-04-15 21:55:04 -0700

    the code draw the simple David star

---
+++ david_star.py
@@ -0,0 +1,19 @@
+import turtle
+
+def david():
+  for step in range(6):
+    turtle.begin_fill()
+    for i in range(3):
+      turtle.forward(50)
+      turtle.left(120)
+    turtle.end_fill()
+
+    turtle.forward(50)
+    turtle.right(60)
+
+turtle.shape("turtle")
+turtle.shapesize(3)
+turtle.color("green", "yellow")
+turtle.speed(10)
+
+turtle.hideturtle()
