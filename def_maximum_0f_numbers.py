047bdcfa993184d9ebcdcc5565ccfea20685026d
nikdimentiy <nikdimentiy@yahoo.com>
2021-05-03 23:08:03 -0700

    Update def_maximum_0f_numbers.py

--- def_maximum_0f_numbers.py
+++ def_maximum_0f_numbers.py
@@ -1,9 +1,18 @@
-def max2(x, y):
+def max_2(x, y):
     if x > y:
         return x
     return y
 
-def max3 (x, y, z):
-    return max2(x, max2(y, z))
 
-print(max3(4,8,12))
+def max_3(x, y, z):
+    return max_2(x, max_2(y, z))
+
+
+print("Enter 3 numbers to calculate the max value of them: ")
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+z = int(input("Enter the third number: "))
+
+result = max_3(x, y, z)
+print("The result of operation is: ", result)
+
