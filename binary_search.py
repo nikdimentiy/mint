31cc08b2df35458f75380f4300f7be7d88f3f457
nikdimentiy@yahoo.com <lord@Lords-MacBook-Air.local>
2021-05-13 16:21:22 -0700

    binary search only works on a sorted list of values

--- binary_search.py
+++ binary_search.py
@@ -1,3 +1,4 @@
+#  classic algorithm of binary search
 def binary_search(user_list, item):
     low = 0
     high = len(user_list) - 1
@@ -14,8 +15,8 @@ def binary_search(user_list, item):
     return None
 
 
-my_list = [1, 3, 5, 7, 9, 56, 76, 32, 90, 89, 34, 21, 67, 82, 100]
+my_list = [1, 3, 5, 7, 9, 56, 76, 32, 90, 89, 34, 21, 67, 82, 100, 4, 8]
+my_list.sort()
 
-print(binary_search(my_list, 3))
+print(binary_search(my_list, 7))
 print(binary_search(my_list, -1))
-
