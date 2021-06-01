45e529a4feb7e50ddfa5cdb3742b115ec34bac7e
nikdimentiy <nikdimentiy@yahoo.com>
2021-05-31 23:48:31 -0700

    Update vowels_2.py

--- vowels_2.py
+++ vowels_2.py
@@ -2,5 +2,8 @@ vowels = set("aeiou")
 word = input("Provide a word to search for vowels: ")
 found = vowels.intersection(set(word))
 
+
+print("Vowels in the word entered: ")
 for vowel in found:
     print(vowel)
+
