40c6db914d7329d53420b60567a5b9a19a2e5b4e
nikdimentiy <nikdimentiy@yahoo.com>
2021-07-08 23:49:28 -0700

    Update merge_sort.c

--- merge_sort.c
+++ merge_sort.c
@@ -1,3 +1,7 @@
+// Merge sort is a sorting technique based on divide and conquer technique
+// With worst-case time complexity being Ο(n log n), it is one of the most respected algorithms
+// Merge sort first divides the array into equal halves and then combines them in a sorted manner
+
 #include <stdio.h>
 
 
