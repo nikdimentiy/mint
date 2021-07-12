faae17b741747e88d826b86652c10e553a754589
nikdimentiy <nikdimentiy@yahoo.com>
2021-07-11 23:19:29 -0700

    added new class Battery and new class ElectricCar

--- car.py
+++ car.py
@@ -30,3 +30,34 @@ class Car():
     def increment_odometer(self, miles):
         """Increases odometer reading with a given increment"""
         self.odometer_reading += miles
+
+
+class Battery():
+    """Simple car battery model"""
+
+    def __init__(self, battery_size=70):
+        """Initialiation of battery attributes"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Displays battery power information"""
+        print("This car has a " + str(self.battery_size) + " -kWh battery.")
+
+    def get_range(self):
+        """Displays an approximate power reserve for the battery"""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full cahrge."
+        print(message)
+
+
+class ElectricCar(Car):
+    """Represents aspects of the car - specific to electric cars"""
+
+    def __init__(self, make, model, year):
+        """Initialize the attributes of the super class, after for subclass"""
+        super().__init__(make, model, year)
+        self.battery = Battery()
