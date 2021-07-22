cfa919ccbcca9b3056b20fb2865a6f668dee60e3
nikdimentiy <nikdimentiy@yahoo.com>
2021-07-22 13:06:21 -0700

    American Standard Code for Information Interchange - added comment

--- ascii_table.c
+++ ascii_table.c
@@ -1,5 +1,8 @@
 // The program show: uppercase and lowercase letter in ASCII table
-// ASCII stands for American Standard Code for Information Interchange
+// American Standard Code for Information Interchange, is a character encoding 
+// standard for electronic communication. ASCII codes represent text in computers, 
+// telecommunications equipment, and other devices. Most modern character-encoding 
+// schemes are based on ASCII, although they support many additional characters
 
 #include <stdio.h>
 
