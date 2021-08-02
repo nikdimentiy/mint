43d850724415f8920c98f18be45f9a4fd311f55c
nikdimentiy <nikdimentiy@yahoo.com>
2021-08-01 22:24:25 -0700

    Update check_password.py
    The function checks the correctness of password entry

--- check_password.py
+++ check_password.py
@@ -1,9 +1,14 @@
-# a small program to check password
-for attempt in range(3, 0, -1):
-    password = int(input("Enter password - only numbers: "
-                         "(you have {} attempts left): ".format(attempt)))
-    if password == 12345:
-        print("Access done!")
-        break
-else:
-    print("Access denied!")
+def check_password():
+    """The function checks the correctness of password entry -
+        for compliance with the sample (the right password)"""
+    for attempt in range(3, 0, -1):
+        password = int(input("Enter password - only numbers: "
+                             "(you have {} attempts left): ".format(attempt)))
+        if password == 12345:
+            print("Access done!")
+            break
+    else:
+        print("Access denied!")
+
+
+check_password()
