93200e9a590c9a8960fb0967dc05beb8d422498d
nikdimentiy <nikdimentiy@yahoo.com>
2021-08-22 22:24:21 -0700

    Update generate_permutations.py

--- generate_permutations.py
+++ generate_permutations.py
@@ -1,5 +1,5 @@
 def find(number, A):
-    """Looks for a number in list A and return - True, if there is such a  number, otherwise - False"""
+    """ Looks for a number in list A and return - True, if there is such a  number, otherwise - False """
     for x in A:
         if number == x:
             return True
@@ -7,6 +7,7 @@ def find(number, A):
 
 
 def generate_permutations(N: int, M: int = -1, prefix=None):
+    """ Generation of all permutations: of N numbers in M positions """
     M = N if M == -1 else M
     prefix = prefix or []
     if M == 0:
