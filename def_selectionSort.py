9fe717cb23249e7b021edcdcf6a6869ab864694b
nikdimentiy <nikdimentiy@yahoo.com>
2021-08-29 17:24:35 -0700

    Update def_selectionSort.py
    added comments of functions

--- def_selectionSort.py
+++ def_selectionSort.py
@@ -1,6 +1,6 @@
 def findSmallest(arr: list):
-    smallest = arr[0]
-    smallest_index = 0
+    smallest = arr[0] # stores the smallest value
+    smallest_index = 0 # stores the index of the smallest value
     for i in range(1, len(arr)):
         if arr[i] < smallest:
             smallest = arr[i]
@@ -8,10 +8,10 @@ def findSmallest(arr: list):
     return smallest_index
 
 
-def selectionSort(arr: list):
+def selectionSort(arr: list): # sort an array
     newArr = []
     for i in range(len(arr)):
-        smallest = findSmallest(arr)
+        smallest = findSmallest(arr) # finds the smallest element in the array, and adds it to the new array
         newArr.append(arr.pop(smallest))
     return newArr
 
