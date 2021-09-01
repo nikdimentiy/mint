a345dc727217e925f811baf40ba88e71e2175d97
nikdimentiy <nikdimentiy@yahoo.com>
2021-08-31 21:37:46 -0700

    Create find_in_string.py

---
+++ find_in_string.py
@@ -0,0 +1,12 @@
+s = input("Enter some phrase or string: ")
+anscnt = 0
+symcnt = {}
+for now in s:
+    if now not in symcnt:
+        symcnt[now] = 0
+    symcnt[now] += 1
+    if symcnt[now] > anscnt:
+        ans = now
+        anscnt = symcnt[now]
+
+print(ans)
