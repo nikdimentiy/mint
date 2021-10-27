a58f49af2c860e132f1a70ba888bc70af89f9764
nikdimentiy <nikdimentiy@yahoo.com>
2021-10-26 21:51:27 -0700

    Create stock_price.go

---
+++ stock_price.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var newPrice, oldPrice, result float64
+
+	println("Enter today's stock price on stock market:")
+	fmt.Scan(&newPrice)
+
+	println("Enter old stock price on stock market: ")
+	fmt.Scan(&oldPrice)
+
+	result = stockPrice(newPrice, oldPrice)
+
+	if result > 0 {
+		fmt.Printf("The share price increased by: %3.3f percent\n", result)
+	} else {
+		fmt.Printf("The stock lost: %3.3f percentage in value\n", result)
+	}
+}
+
+func stockPrice(newPrice, oldPrice float64) float64 {
+	qDif := newPrice - oldPrice
+	growthPercentage := (qDif / oldPrice) * 100
+	return growthPercentage
+}
