154c5b381eee40d99b23c0ec0601366291724d80
nikdimentiy <nikdimentiy@yahoo.com>
2021-12-12 23:32:56 -0800

    added hand figures (art)

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,32 @@
 import random
 
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
 game_images = [rock, paper, scissors]
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
@@ -21,3 +48,4 @@ elif user_choice > computer_choice:
   print("You win!")
 elif computer_choice == user_choice:
   print("It's a draw")
+
