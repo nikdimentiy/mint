be1d3aced1e0ae91f679ef577586e50f279156cc
nikdimentiy <nikdimentiy@yahoo.com>
2022-01-11 23:20:35 -0800

    Update square_complexity.py
    simplicity code - very good code

--- Finding in Sequence/square_complexity.py
+++ Finding in Sequence/square_complexity.py
@@ -1,12 +1,12 @@
-sequence_input = input()
-answer = ""
-answer_count = 0
-for i in range(len(sequence_input)):
-    now_count = 0
-    for j in range(len(sequence_input)):
-        if sequence_input[j] == sequence_input[i]:
-            now_count += 1
-    if now_count > answer_count:
-        ans = sequence_input[i]
-        answer_count = now_count
-print(answer)
+s = input()
+ans = ""
+anscnt = 0
+for i in range(len(s)):
+    nowcnt = 0
+    for j in range(len(s)):
+        if s[i] == s[j]:
+            nowcnt += 1
+    if nowcnt > anscnt:
+        ans = s[i]
+        anscnt = nowcnt
+print(ans)
