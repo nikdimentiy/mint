d0dde1c49790ae2c6536d916641bdea7e82d9009
nikdimentiy <nikdimentiy@yahoo.com>
2022-01-14 22:55:04 -0800

    Update n_complexity.py

--- Finding in Sequence/n_complexity.py
+++ Finding in Sequence/n_complexity.py
@@ -1,9 +1,11 @@
+#  n complexity of the algorithm (where n - number of sequence elements)
+
 s = input()
 anscnt = 0
-symcnt = {}
+symcnt = {} # using dictionary 
 for now in s:
     if now not in symcnt:
-        symcnt[now] = 0
+        symcnt[now] = 0 # added value in dict using key=now
     symcnt[now] += 1
     if symcnt[now] > anscnt:
         ans = now
