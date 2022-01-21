143751f54f854445ce521a06e462ce6f72d305b0
nikdimentiy <nikdimentiy@yahoo.com>
2022-01-20 23:00:50 -0800

    Update file_reader.py

--- Python_CSV/file_reader.py
+++ Python_CSV/file_reader.py
@@ -1,5 +1,5 @@
-FILE_PATH = '/home/polo/Documents/Workspace/Python/quotes.csv'
+FILE_PATH = '/home/polo/Documents/Workspace/Python/quotes.csv'  # file path in my Linux desctop
 
 with open(FILE_PATH) as file_object:
     for line in file_object:
-        print(line.rstrip())
+        print(line.rstrip()) # strip symbol of new line after iteration of the 'for' loop
