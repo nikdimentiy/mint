e9a9289f2d55659090af39e99d6ff6cf6553d198
nikdimentiy <nikdimentiy@yahoo.com>
2022-02-16 19:10:02 -0800

    Update names.py

--- Testing/names.py
+++ Testing/names.py
@@ -1,14 +1,14 @@
-import unittest
 from name_function import get_formatted_name
+"""This function asks for a username and return a neatly formatted form"""
 
+print("Enter 'q' at any time to quit.")
+while True:
+    first = input("\nPlease give me a first name:")
+    if first == 'q':
+        break
+    last = input("Please gime me a last name: ")
+    if last == 'q':
+        break
 
-class NameTestCase(unittest.TestCase):
-    """Tests for function -> (name_function)"""
-
-    def test_first_last_name(self):
-        """names like: Janis and Joplin -> working right?"""
-        formated_name = get_formatted_name('janis', 'joplin')
-        self.assertEqual(formated_name, "Janis Joplin")
-
-
-unittest.main()
+    formatted_name = get_formatted_name(first, last)
+    print("\tNeatly formatted name: " + formatted_name + ".")
