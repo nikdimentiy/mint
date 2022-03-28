98c98edaa58a74232eaae8a66c39409a61465a63
nikdimentiy <nikdimentiy@yahoo.com>
2022-03-27 23:12:17 -0700

    Create discriminant.py

---
+++ discriminant.py
@@ -0,0 +1,7 @@
+# <-- This program calculates the roots of the quadratic equation
+#     and displays them on the screen (in ascending order) -->
+
+print("Enter the components (coefficients) of the polynominal")
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
