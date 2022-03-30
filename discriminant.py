48591dcb8757c75b226340c3bc54ab8d9beab8e9
nikdimentiy <nikdimentiy@yahoo.com>
2022-03-29 23:16:26 -0700

    Update discriminant.py

--- discriminant.py
+++ discriminant.py
@@ -1,29 +1,27 @@
-# <-- This program calculates the roots of the quadratic equation
-#     and displays them on the screen (in ascending order) -->
-
 from math import sqrt
 
+""" <-- This program calculates the roots of the quadratic equation
+    and displays them on the screen (in ascending order) --> """
 
-print(" *** Enter the components (coefficients) of the polynominal *** ")
+print(" *** Enter the components (coefficients) of the polynomial *** ")
 print()
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
 c = int(input("Enter c: "))
 
+discriminant = (b ** 2 - 4 * a * c)
 if a == 0:
     if b != 0:
         print(-c / b)
-    if b == 0 and c == 0:
-        print("Infinity number of solutions")
-else:
-    d = b ** 2 - 4 * a * c
-    if d == 0:
-        x1 = -b / (2 * a)
-        print(x1)
-    elif d > 0:
-        x1 = (-b - sqrt(d)) / (2 * a)
-        x2 = (-b + sqrt(d)) / (2 * a)
-        if x1 < x2:
-            print(x1, x2)
-        else:
-            print(x2, x1)
+    if c == 0 and b == 0:
+        print("Infinity number of solution.")
+elif discriminant == 0:
+    x1 = -b / (2 * a)
+    print(x1)
+elif discriminant > 0:
+    x1 = (-b + sqrt(discriminant)) / 2 * a
+    x2 = (-b - sqrt(discriminant)) / 2 * a
+    if x1 > x2:
+        print(x2, x1)
+    else:
+        print(x1, x2)
