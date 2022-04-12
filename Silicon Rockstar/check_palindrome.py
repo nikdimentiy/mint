66c759c4aed666d71101426764f4125e2f3108f0
nikdimentiy <nikdimentiy@yahoo.com>
2022-04-11 22:32:28 -0700

    Add files via upload

---
+++ Silicon Rockstar/check_palindrome.py
@@ -0,0 +1,10 @@
+
+# the code analyzes whether the entered value is a palindrome (short version)
+
+string_1 = input("Enter the needed value: ")
+reverse_string = string_1[::-1]
+
+if string_1 == reverse_string:
+    print("YES - entered value is palindrome!")
+else:
+    print("NO - this value in not palindrome!")
---
+++ Silicon Rockstar/check_palindrome_while.py
@@ -0,0 +1,16 @@
+
+# the tiny code: analyzes whether the entered value is a palindrome
+s = input()
+# let's compare the elements of the sequence in pairs: the first with the last, the second with the penultimate...
+i = 0
+j = len(s) - 1 # calculate the length of the sequence and take the last element
+is_polindrom = True
+while i < j: # step by step compare the elements of the sequence by direction to the conditional middle
+    if s[i] != s[j]:
+        is_polindrom = False
+    i += 1
+    j -= 1
+if is_polindrom:
+    print("YES - entered value is palindrome!")
+else:
+    print("NO - this value in not palindrome!!!!")
