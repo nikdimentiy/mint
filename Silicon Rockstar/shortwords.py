6579e428f023e867fe5ba4ef17c61724a524cf64
nikdimentiy <nikdimentiy@yahoo.com>
2022-04-15 23:18:27 -0700

    Add files via upload

---
+++ Silicon Rockstar/shortwords.py
@@ -0,0 +1,10 @@
+def shortwords(words):
+    minlen = len(words[0])
+    for word in words:
+        if len(word) < minlen:
+            minlen = len(words)
+    ans = []
+    for word in words:
+        if len(word) == minlen:
+            ans.append(word)
+    return ' '.join(ans)
