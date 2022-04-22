aef5d0fae1c699bbf29533cac79b654ed0f11731
nikdimentiy <nikdimentiy@yahoo.com>
2022-04-22 11:14:16 -0700

    Update common_sequence_character.py

--- Silicon Rockstar/common_sequence_character.py
+++ Silicon Rockstar/common_sequence_character.py
@@ -10,3 +10,19 @@ for i in range(len(my_string)):
             answer = my_string[i]
             answer_count = now_count
 print(answer)  # Big O notation = O(n^2)
+
+######################################################################################
+my_string = input()
+answer = ""
+answer_count = 0
+for letter in set(my_string):
+    now_count = 0
+    for j in range(len(my_string)):
+        if letter == my_string[j]:
+            now_count += 1
+        if now_count > answer_count:
+            answer = letter
+            answer_count = now_count
+print(answer)  # Big O notation = O(n K), where "K" is the number of different symbols
+
+#####################################################################################
