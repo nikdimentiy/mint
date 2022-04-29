65e42320f961b4ee603524e19825074cc42e2a18
nikdimentiy <nikdimentiy@yahoo.com>
2022-04-28 23:42:16 -0700

    Add files via upload

---
+++ Silicon Rockstar/silicon.py
@@ -0,0 +1,40 @@
+import pprint as pretty
+
+silicon_rockstar = {}
+silicon_rockstar["Dmitriy"] = {"Name: ": "Dmitriy Nikey",
+                               "Gender: ": "Male",
+                               "Programming language: ": "Go",
+                               "State: ": "California",
+                               "City: ": "Mountain View"}
+
+silicon_rockstar["Alexsandr"] = {"Name: ": "Alexandr Trump",
+                                 "Gender: ": "Male",
+                                 "Programming languge: ": "JavaScript",
+                                 "State: ": "Nevada",
+                                 "City: ": "Reno"}
+
+silicon_rockstar["Nadiya"] = {"Name: ": "Nadiya Polonnaya",
+                              "Gender: ": "Female",
+                              "Programming language: ": "Python",
+                              "State: ": "New-Jersey",
+                              "City: ": "Newark"}
+
+silicon_rockstar["Maria"] = {"Name: ": "Maria Orlova",
+                             "Gender: ": "Female",
+                             "Programming language: ": "Java",
+                             "State: ": "Massachusetts",
+                             "City: ": "Boston"}
+
+silicon_rockstar["Svetlana"] = {"Name: ": "Svetlana Thompson",
+                                "Gender: ": "Female",
+                                "Programming language: ": "Ruby",
+                                "State: ": "Delaware",
+                                "City: ": "Wilmington"}
+
+silicon_rockstar["Taras"] = {"Name: ": "Taras Klinton",
+                             "Gender: ": "Male",
+                             "Programming language: ": "Python",
+                             "State: ": "New York",
+                             "City: ": "Albany"}
+
+print(pretty.pprint(silicon_rockstar))
