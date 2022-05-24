fc2f3574514b0bd296bea1c149ddfd81d1fbb881
nikdimentiy <nikdimentiy@yahoo.com>
2022-05-23 20:32:05 -0700

    Add files via upload

---
+++ CodeWars/exitst_triangle.py
@@ -0,0 +1,7 @@
+def is_triangle(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return False
+    if a + b > c and a + c > b and c + b > a:
+        return True
+    else:
+        return False
