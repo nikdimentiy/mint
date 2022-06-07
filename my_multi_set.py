17e88900d1f3babbb7fdd4c8dd95e2142f6a1044
nikdimentiy <nikdimentiy@yahoo.com>
2022-06-06 22:44:45 -0700

    Update my_multi_set.py
    add comments

--- my_multi_set.py
+++ my_multi_set.py
@@ -1,18 +1,20 @@
+#  my realization  of hash table (hash function)
+
 setsize = 10
 myset = [[] for _ in range(setsize)]
 
-
+#  operation add element in set
 def add(x):
     myset[x % setsize].append(x)
 
-
+#  operation find element in set
 def find(x):
     for now in myset[x % setsize]:
         if now == x:
             return True
     return False
 
-
+#  operation delete element in set
 def delete(x):
     xlist = myset[x % setsize]
     for i in range(len(xlist)):
