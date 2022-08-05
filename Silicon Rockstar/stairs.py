9490b1df9662cb92cd2349fc1326c7052814f4f2
nikdimentiy <nikdimentiy@yahoo.com>
2022-08-05 02:10:02 -0700

    Update stairs.py
    some improvements in code

--- Silicon Rockstar/stairs.py
+++ Silicon Rockstar/stairs.py
@@ -1,8 +1,8 @@
-# Given a natural number -> output a ladder of n steps, the i-th step consists of numbers from 1 to i without spaces.
+# Given a natural number(1-9) -> output a ladder of n steps, 
+# the i-th step consists of numbers from 1 to i without spaces.
 
 n = int(input())
 k = 1
-i = 1
 while k <= n:
     for i in range(1, k + 1):
         print(i, end="")
