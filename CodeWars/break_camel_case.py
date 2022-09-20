3288c05cdf8d82730b2628d4bc554af3feebeffa
nikdimentiy <nikdimentiy@yahoo.com>
2022-09-19 21:13:38 -0700

    Update break_camel_case.py

--- CodeWars/break_camel_case.py
+++ CodeWars/break_camel_case.py
@@ -1,5 +1,4 @@
-from unittest import result
-
+# Complete the solution so that the function will break up camel casing, using a space between words.
 
 def solution(s):
     return "".join([" " + c if c.isupper() else c for c in s])
