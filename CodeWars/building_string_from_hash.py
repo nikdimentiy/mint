889b2a5f0d80f33d105687ab7d84f08c80ca2681
nikdimentiy <nikdimentiy@yahoo.com>
2022-09-20 23:48:29 -0700

    Update building_string_from_hash.py
    added requirements of task

--- CodeWars/building_string_from_hash.py
+++ CodeWars/building_string_from_hash.py
@@ -1,3 +1,8 @@
+# Complete the solution so that it takes the object  or hash,
+# passed in and generates a human readable string from its key/value pairs.
+
+# The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
+
 
 def solution(pairs):
     return ",".join(sorted(["{} = {}".format(k, v) for k, v in pairs.items()]))
