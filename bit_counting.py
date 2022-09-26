d9af57bc0004ff2ad9b78e727245554db61db236
nikdimentiy <nikdimentiy@yahoo.com>
2022-09-25 23:58:39 -0700

    Update bit_counting.py

--- bit_counting.py
+++ bit_counting.py
@@ -1,2 +1,7 @@
-	def countBits(n):
+# Write a function that takes an integer as input,
+# and returns the number of bits that are equal to one in the binary representation of that number.
+
+
+def countBits(n):
     return bin(n).count("1")
+
