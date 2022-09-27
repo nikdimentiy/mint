e397e6afee55b61a6ec528a1411534d384b5ccf1
nikdimentiy <nikdimentiy@yahoo.com>
2022-09-26 23:41:04 -0700

    Update ascii_letters_from_number.py

--- CodeWars/ascii_letters_from_number.py
+++ CodeWars/ascii_letters_from_number.py
@@ -1,3 +1,13 @@
+# You have to create a function that converts integer given as string into ASCII uppercase letters.
+
+# All ASCII characters have their numerical order in table.
+
+# For example,
+
+# from ASCII table, character of number 65 is "A".
+
+# Numbers will be next to each other, So you have to split given number to two digit long integers.
+
 
 def convert(number):
-    return "".join([chr(int(number[e:e+2])) for e in range(0, len(number), 2)])
+    return "".join([chr(int(number[e:e+2])) for e in range(0, len(number), 2)]))
