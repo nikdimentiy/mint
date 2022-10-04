bfc4471efd6dcacb26a1eff2b6840a0463368781
nikdimentiy <nikdimentiy@yahoo.com>
2022-10-03 19:59:30 -0700

    Add files via upload

---
+++ CodeWars/to_camel_case.py
@@ -0,0 +1,11 @@
+# Complete the method/function so that it converts dash/underscore delimited words into camel casing.
+# The first word within the output should be capitalized only if the original word was capitalized
+# (known as Upper Camel Case, also often referred to as Pascal case).
+
+
+def to_camel_case(text):
+    s = text.replace("-", " ").replace("_", " ")
+    s = s.split()
+    if len(text) == 0:
+        return text
+    return s[0] + ''.join(i.capitalize() for i in s[1:])
