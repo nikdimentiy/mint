026b5d33e33b1fe2cf77b3b379605cd40e7883fb
nikdimentiy <nikdimentiy@yahoo.com>
2022-10-08 23:22:45 -0700

    Update winner.py

--- CodeWars/winner.py
+++ CodeWars/winner.py
@@ -1,3 +1,9 @@
+# Create a function that returns the name of the winner in a fight between two fighters.
+# Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+# Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+# Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects
+
+
 def declare_winner(fighter1, fighter2, first_attacker):
     cur, opp = (fighter1, fighter2) if first_attacker == fighter1.name else (fighter2, fighter1)
     while cur.health > 0:        
