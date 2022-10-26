80f7324af790f0f1d0bdd6f67eebd63378242625
nikdimentiy <nikdimentiy@yahoo.com>
2022-10-25 23:01:57 -0700

    Add files via upload

---
+++ CodeWars/decode_cod_morze2.py
@@ -0,0 +1,8 @@
+def decodeMorse(morse_sequence):
+    words = []
+    for morse_word in morse_sequence.split('   '):
+        word = ''.join(MORSE_CODE.get(morse_char, '')
+                       for morse_char in morse_word.split(' '))
+        if word:
+            words.append(word)
+    return ' '.join(words)
