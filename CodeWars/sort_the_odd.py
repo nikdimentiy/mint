447db5432a2e3390f2d26233d38fb3e85971be21
nikdimentiy <nikdimentiy@yahoo.com>
2022-11-30 20:35:10 -0800

    Update sort_the_odd.py

--- CodeWars/sort_the_odd.py
+++ CodeWars/sort_the_odd.py
@@ -1,6 +1,7 @@
 def sort_array(source_array):
-    odds = sorted([n for n in source_array if n % 2 != 0])
-    for n in source_array:
-        if n % 2 == 0:
-            odds.insert(source_array.index(n), n)
-    return odds
+    result = sorted([l for l in source_array if l % 2 == 1])
+    for index, item in enumerate(source_array):
+        if item % 2 == 0:
+            result.insert(index, item)
+    return result
+
