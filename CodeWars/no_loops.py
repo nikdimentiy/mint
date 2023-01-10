0388803de502d8a23a1ffac81e0e75b3d8fe4c45
nikdimentiy <nikdimentiy@yahoo.com>
2023-01-09 18:48:58 -0800

    Add files via upload

---
+++ CodeWars/no_loops.py
@@ -0,0 +1,11 @@
+# You will be given an array (a) and a limit value (limit).
+# You must check that all values in the array are below or equal to the limit value.
+#  If they are, return True. Else, return False.
+
+# You can assume all values in the array are numbers.
+
+# Do not use loops. Do not modify input array.
+
+
+def check(a, x):
+    return x in a
