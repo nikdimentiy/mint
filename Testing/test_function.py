153cd125ea81f65d55e3de1ec5c0879bc769a1b1
nikdimentiy <nikdimentiy@yahoo.com>
2023-01-10 19:08:23 -0800

    Update test_function.py

--- Testing/test_function.py
+++ Testing/test_function.py
@@ -1,16 +1,29 @@
 def insert_sort(A):
     """insert sort array (list)"""
-    pass
+    N = len(A)
+    for top in range(1, N):
+        k = top
+        while k > 0 and A[k-1] > A[k]:
+            A[k], A[k-1] = A[k-1], A[k]
+            k -= 1
 
 
 def select_sort(A):
     """select sort array (list)"""
-    pass
+    N = len(A)
+    for pos in range(0, N - 1):
+        for k in range(pos + 1, N):
+            if A[k] < A[pos]:
+                A[k], A[pos] = A[pos], A[k]
 
 
 def bubble_sort(A):
     """bubble sort array (list)"""
-    pass
+    N = len(A)
+    for bypass in range(1, N):
+        for k in range(0, N-bypass):
+            if A[k] > A[k+1]:
+                A[k], A[k+1] = A[k+1], A[k]
 
 
 def test_sort(sort_algorithm):
