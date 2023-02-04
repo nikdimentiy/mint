04752426bf41b1f63a8f388f476ba886ccc5fad1
nikdimentiy <nikdimentiy@yahoo.com>
2023-02-04 12:11:05 -0800

    dereferencing void pointer

---
+++ CS50/void_pointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int x = 33;
+    float y = 12.4;
+    char c = 'a';
+    void *ptr;
+  
+    ptr = &x;
+    printf("void ptr points to %d\n", *((int*)ptr));
+    ptr = &y;
+    printf("void ptr points to %f\n", *((float *)ptr));
+    ptr = &c;
+    printf("void ptr points to %c\n", *((char *)ptr));
+
+    return 0;
+}
