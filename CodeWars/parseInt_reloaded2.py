c982e25b93693137bb7613c41cc4688ea1794263
nikdimentiy <nikdimentiy@yahoo.com>
2023-02-22 14:42:27 -0800

    another variant of solution --> CodeWars task

---
+++ CodeWars/parseInt_reloaded2.py
@@ -0,0 +1,33 @@
+# In this kata we want to convert a string into an integer.
+# The strings simply represent the numbers in words.
+
+# Examples:
+
+# "one" => 1
+# "twenty" => 20
+# "two hundred forty-six" => 246
+# "seven hundred eighty-three thousand nine hundred and nineteen" => 783919
+
+
+ONES = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen',
+        'eighteen', 'nineteen']
+TENS = ['twenty', 'thirty', 'forty', 'fifty',
+        'sixty', 'seventy', 'eighty', 'ninety']
+
+
+def parse_int(string):
+    print(string)
+    numbers = []
+    for token in string.replace('-', ' ').split(' '):
+        if token in ONES:
+            numbers.append(ONES.index(token))
+        elif token in TENS:
+            numbers.append((TENS.index(token) + 2) * 10)
+        elif token == 'hundred':
+            numbers[-1] *= 100
+        elif token == 'thousand':
+            numbers = [x * 1000 for x in numbers]
+        elif token == 'million':
+            numbers = [x * 1000000 for x in numbers]
+    return sum(numbers)
