add66a50d605a31730b0a2ae3741d0cc1e255aad
nikdimentiy <nikdimentiy@yahoo.com>
2023-02-27 17:57:07 -0800

    another variant of solution CodeWars task

---
+++ CodeWars/is_prime2.py
@@ -0,0 +1,18 @@
+def is_prime(n):
+    # Handle special cases
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+
+    # Loop through possible divisors (6k +/- 1)
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+
+    # If we get here, n is prime
+    return True
