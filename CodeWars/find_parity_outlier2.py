08a90008a7b75194aa54b94e5de4662bf28fe5fc
nikdimentiy <nikdimentiy@yahoo.com>
2023-03-05 15:28:27 -0800

    another variant of solution CodeWars task

---
+++ CodeWars/find_parity_outlier2.py
@@ -0,0 +1,18 @@
+# You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+# The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
+# Write a method that takes the array as an argument and returns this "outlier" N.
+
+
+def find_outlier(integers):
+    listEven = []
+    listOdd = []
+    for n in integers:
+        if n % 2 == 0:
+            listEven.append(n)
+        else:
+            listOdd.append(n)
+
+    if len(listEven) == 1:
+        return listEven[0]
+    else:
+        return listOdd[0]
