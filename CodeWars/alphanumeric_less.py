68c2dea8b5adcfb4402d1e3a3744d5137b169805
nikdimentiy <nikdimentiy@yahoo.com>
2023-03-23 21:02:19 -0700

    Add files via upload

---
+++ CodeWars/alphanumeric_less.py
@@ -0,0 +1,16 @@
+import re
+
+
+def solution(s1, s2):
+    return tokenize(s1) < tokenize(s2)
+
+
+def tokenize(s):
+    tokens = re.findall('\d+|.', s)
+    return (
+        [
+            (0, int(token)) if token.isdigit() else (1, token)
+            for token in tokens
+        ],
+        [-len(token) for token in tokens]
+    )
