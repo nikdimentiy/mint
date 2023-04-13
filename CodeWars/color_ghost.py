4bde1b08cea216bfaae7b4742abfbef050dbb620
nikdimentiy <nikdimentiy@yahoo.com>
2023-04-12 23:38:24 -0700

    Create color_ghost.py

---
+++ CodeWars/color_ghost.py
@@ -0,0 +1,7 @@
+# CodeWars task: solution --> https://www.codewars.com/kata/53f1015fa9fe02cbda00111a/python
+
+import random
+
+class Ghost(object):
+  def __init__(self):
+    self.color = random.choice(["white", "yellow", "purple", "red"])
