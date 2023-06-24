0dcb186366d5506805db003983d451d9000721d0
nikdimentiy <nikdimentiy@yahoo.com>
2023-06-23 20:41:21 -0700

    Update paddle.py
    add comments

--- Pong Game/paddle.py
+++ Pong Game/paddle.py
@@ -1,20 +1,24 @@
 from turtle import Turtle
 
-
 class Paddle(Turtle):
-    
+    """A class to represent the paddle for a pong game."""
+
     def __init__(self, position):
+        """Initialize the paddle with a square shape, white color and a given position."""
         super().__init__()
         self.shape("square")
         self.color("white")
-        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.shapesize(stretch_wid=5, stretch_len=1) # make the paddle longer and thinner
         self.penup()
         self.goto(position)
 
     def go_up(self):
-        new_y = self.ycor() + 20
-        self.goto(self.xcor(), new_y)
+        """Move the paddle up by 20 pixels."""
+        new_y = self.ycor() + 20 # get the current y-coordinate and add 20
+        self.goto(self.xcor(), new_y) # go to the new position
 
     def go_down(self):
-        new_y = self.ycor() - 20
-        self.goto(self.xcor(), new_y)
+        """Move the paddle down by 20 pixels."""
+        new_y = self.ycor() - 20 # get the current y-coordinate and subtract 20
+        self.goto(self.xcor(), new_y) # go to the new position
+
