88c34313ce8a9af2ccc52dc9f54f0dd00cb86176
nikdimentiy <nikdimentiy@yahoo.com>
2023-06-23 20:39:32 -0700

    Update scoreboard.py
    add comments

--- Pong Game/scoreboard.py
+++ Pong Game/scoreboard.py
@@ -1,18 +1,20 @@
 from turtle import Turtle
 
-
 class Scoreboard(Turtle):
+    """A class to represent the scoreboard for a pong game."""
 
     def __init__(self):
+        """Initialize the scoreboard with white color and zero scores."""
         super().__init__()
         self.color("white")
         self.penup()
         self.hideturtle()
-        self.l_score = 0
-        self.r_score = 0
+        self.l_score = 0 # score for the left player
+        self.r_score = 0 # score for the right player
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        """Clear the previous scores and write the current scores on the screen."""
         self.clear()
         self.goto(-100, 200)
         self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
@@ -20,9 +22,11 @@ class Scoreboard(Turtle):
         self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
 
     def l_point(self):
+        """Increase the score for the left player by one and update the scoreboard."""
         self.l_score += 1
         self.update_scoreboard()
 
     def r_point(self):
+        """Increase the score for the right player by one and update the scoreboard."""
         self.r_score += 1
         self.update_scoreboard()
