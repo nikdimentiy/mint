6883305738af77633de38ec2a73e97f6eb5d0750
nikdimentiy <nikdimentiy@yahoo.com>
2023-08-08 20:50:57 -0700

    Update selection_sort.py
    add comments and doc string

--- Algorithms/selection_sort.py
+++ Algorithms/selection_sort.py
@@ -1,4 +1,18 @@
+"""
+This program implements the selection sort algorithm.
+
+The selection sort algorithm works by repeatedly finding the smallest element in the unsorted array and moving it to the front of the array.
+
+Args:
+    array: The array to be sorted.
+
+Returns:
+    The sorted array.
+"""
+
 def findSmallest(array):
+    """Finds the smallest element in the array."""
+
     smallest = array[0]
     smallest_index = 0
     for i in range(1, len(array)):
@@ -9,11 +23,22 @@ def findSmallest(array):
 
 
 def selectionSort(array):
+    """Sorts the array using the selection sort algorithm."""
+
     newArray = []
     for i in range(len(array)):
-        small_element = findSmallest(array)
-        newArray.append(array.pop(small_element))
+        # Find the smallest element in the unsorted array.
+        smallest_index = findSmallest(array)
+
+        # Remove the smallest element from the unsorted array.
+        smallest = array.pop(smallest_index)
+
+        # Append the smallest element to the sorted array.
+        newArray.append(smallest)
+
     return newArray
 
 
-print(selectionSort([3, 9, 8, 4, 2, 0, 7, 1]))
+# Test the function.
+array = [3, 9, 8, 4, 2, 0, 7, 1]
+print(selectionSort(array))
