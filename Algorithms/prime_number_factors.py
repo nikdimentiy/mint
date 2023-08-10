e854841b5c77ac9b77fbb4e6372a33af15f8bfbb
nikdimentiy <nikdimentiy@yahoo.com>
2023-08-09 22:09:53 -0700

    Update prime_number_factors.py
    create doc string and comments

--- Algorithms/prime_number_factors.py
+++ Algorithms/prime_number_factors.py
@@ -1,15 +1,34 @@
 def print_prime_factors(number):
-    # Start with two, which is the first prime
+    """
+    Prints the prime factors of a given number.
+
+    This function takes an integer 'number' as input and prints its prime factors.
+
+    Parameters:
+    number (int): The number for which prime factors need to be printed.
+
+    Returns:
+    None
+    """
+
+    # Start with the smallest prime factor, which is 2
     factor = 2
-    # Keep going until the factor is larger than the number
+
+    # Keep looping until the factor becomes larger than the number
     while factor <= number:
-        # Check if factor is a divisor of number
+
+        # Check if the current factor is a divisor of the number
         if number % factor == 0:
+
+            # If it is a divisor, print it as a prime factor
             print(factor)
+
+            # Divide the number by the current factor to reduce it
             number = number / factor
         else:
-            # If it's not, increment the factor by one
+            # If the current factor is not a divisor, move to the next one
             factor += 1
 
-
+# Call the function with an example number
 print_prime_factors(37)
+
