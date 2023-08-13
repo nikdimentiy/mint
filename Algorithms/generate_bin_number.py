e28448c56da5eccee351be1f23be0b03b44dc48e
nikdimentiy <nikdimentiy@yahoo.com>
2023-08-12 23:08:49 -0700

    Update generate_bin_number.py
    create doc string and comments

--- Algorithms/generate_bin_number.py
+++ Algorithms/generate_bin_number.py
@@ -1,26 +1,54 @@
-def gen_bin(M, prefix=''):
-    if M == 0:
-        print(prefix)
-        return
-    for digits in "0", "1":
-        gen_bin(M-1, prefix+digits)
-
-
 def generate_number(N: int, M: int, prefix=None):
-    """ genaration of numbers in N (number system)--> N <= 10
-        N = number system
-        M = length of number
     """
-    prefix = prefix or []
+    Generate all possible numbers in N (number system) where N <= 10.
+
+    Args:
+        N: The number system.
+        M: The length of the number.
+        prefix: A list of digits that have already been generated.
+
+    Returns:
+        None.
+    """
+
+    # Check if the number is complete.
+
     if M == 0:
         print(prefix)
         return
+
+    # Iterate over all possible digits.
+
     for digit in range(N):
+        # Add the digit to the prefix.
         prefix.append(digit)
+
+        # Recurse to generate the next number.
         generate_number(N, M - 1, prefix)
+
+        # Remove the digit from the prefix.
         prefix.pop()
 
 
-# gen_bin(12)
+def gen_bin(M, prefix=''):
+    """Generate all possible binary numbers of length M."""
+
+    # This is a helper function for generate_number.
+
+    if M == 0:
+        print(prefix)
+        return
+
+    for digit in ['0', '1']:
+        gen_bin(M - 1, prefix + digit)
+
+
+# Example usage
+
+print('Generate all binary numbers of length 8:')
 generate_number(2, 8)
+
+print('Generate all binary numbers of length 12:')
+gen_bin(12)
+
 
