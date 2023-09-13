84d9ced776a0d054a9e654a323027bc6d6cc0d39
nikdimentiy <nikdimentiy@yahoo.com>
2023-09-12 21:50:58 -0700

    Update sample_1.py

--- Turtle Sample/sample_1.py
+++ Turtle Sample/sample_1.py
@@ -1,6 +1,17 @@
+# Import the necessary modules
 from turtle import Turtle, Screen
 
+# Create a new Turtle named 'timmy'
 timmy = Turtle()
+
+# Create a Screen object named 'screen'
 screen = Screen()
 
+# Use the 'exitonclick' method to wait for a click on the screen before closing it.
+# This allows the user to interact with the drawing created by the Turtle.
 screen.exitonclick()
+
+# This script initializes a Turtle named 'timmy' and a Screen named 'screen'.
+# The 'exitonclick' method is called on the 'screen' object to wait for a click
+# from the user to close the turtle graphics window.
+
