e4e1ba4f21473966d550e47d9b6dd4e99665a201
nikdimentiy <nikdimentiy@yahoo.com>
2023-09-13 09:42:36 -0700

    Update apple_boxes.py

--- CodeWars/apple_boxes.py
+++ CodeWars/apple_boxes.py
@@ -6,7 +6,29 @@
 # Your task is to calculate the difference between the number of red apples and the number of yellow apples.
 
 
-def solution(k):
-    yellow_apples = sum(i * i for i in range(1, k+1, 2))
-    red_apples = sum(i * i for i in range(2, k+1, 2))
+def calculate_apple_difference(k):
+    """
+    Calculate the difference between the number of red apples and yellow apples in apple boxes.
+
+    Args:
+        k (int): The number of apple boxes.
+
+    Returns:
+        int: The difference between the number of red apples and yellow apples.
+
+    Explanation:
+    - Yellow apples are in boxes with odd sizes (1x1, 3x3, 5x5, ...).
+    - Red apples are in boxes with even sizes (2x2, 4x4, 6x6, ...).
+
+    The function calculates the number of red and yellow apples in the boxes and returns the difference.
+    """
+    yellow_apples = sum(i * i for i in range(1, k + 1, 2))
+    red_apples = sum(i * i for i in range(2, k + 1, 2))
     return red_apples - yellow_apples
+
+# Driver code to test the function
+if __name__ == "__main__":
+    k = int(input("Enter the number of apple boxes (k): "))
+    apple_difference = calculate_apple_difference(k)
+    print(f"The difference between the number of red and yellow apples is: {apple_difference}")
+
