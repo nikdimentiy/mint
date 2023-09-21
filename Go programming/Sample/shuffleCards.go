dd2565d36d18f91718559acf9d42e537c6323b8a
nikdimentiy <nikdimentiy@yahoo.com>
2023-09-20 20:18:01 -0700

    Update shuffleCards.go

--- Go programming/Sample/shuffleCards.go
+++ Go programming/Sample/shuffleCards.go
@@ -9,12 +9,14 @@ import (
 // shuffle randomly shuffles a slice of strings.
 // It takes a slice of cards as input and shuffles them using the Fisher-Yates algorithm.
 func shuffle(cards []string) {
-	rand.Seed(time.Now().UnixNano())
+	// Create a new source with a specific seed based on the current time
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
 
 	// Iterate through the cards slice
 	for i := range cards {
 		// Generate a random number between 0 and the length of the cards slice
-		randomNumber := rand.Intn(len(cards))
+		randomNumber := r.Intn(len(cards))
 
 		// Swap the current card with the card at the random position
 		cards[i], cards[randomNumber] = cards[randomNumber], cards[i]
