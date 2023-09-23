3f73131404372edbf4746004d2d523098290de21
nikdimentiy <nikdimentiy@yahoo.com>
2023-09-23 16:20:06 -0700

    Update copy_slice.go

--- Go programming/copy_slice.go
+++ Go programming/copy_slice.go
@@ -1,29 +1,30 @@
+// This program demonstrates the use of slices in Go.
+
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 func main() {
-	// Create an underlying array with 5 elements
-	underlyingArray := [5]string{"a", "b", "c", "d", "e"}
+    // Create an underlying array.
+    underlyingArray := [5]string{"a", "b", "c", "d", "e"}
 
-	// Create a slice that references a portion of the underlying array
-	// Slice from index 2 (inclusive) to index 4 (exclusive)
-	slice1 := underlyingArray[2:4]
+    // Create a slice from the underlying array, starting at index 2 and ending at index 4 (exclusive).
+    slice1 := underlyingArray[2:4]
 
-	// Create a new slice with the same length as the underlying array
-	// Copy the elements from the underlying array to the new slice
-	slice2 := make([]string, len(underlyingArray))
-	copy(slice2, underlyingArray[:])
+    // Create a new slice with the same length as the underlying array.
+    slice2 := make([]string, len(underlyingArray))
 
-	// Modify the first two elements of slice1
-	slice1[0] = "hello"
-	slice1[1] = "developer"
+    // Copy the elements of the underlying array to the new slice.
+    len_array := copy(slice2, underlyingArray[:])
 
-	// Print the contents of slice1
-	fmt.Println(slice1) // Output: [hello developer]
+    // Modify the elements of the first slice.
+    slice1[0] = "hello"
+    slice1[1] = "developer"
 
-	// Print the contents of slice2
-	fmt.Println(slice2) // Output: [a b c d e]
+    // Print the contents of the two slices and the copied length.
+    fmt.Println(slice1) // Output: [hello developer]
+    fmt.Println(slice2) // Output: [c d c d e]
+    fmt.Println(len_array)
 }
