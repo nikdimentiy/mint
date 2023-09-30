dda2c3df445593380d26ab7ef25d212a32a7866e
nikdimentiy <nikdimentiy@yahoo.com>
2023-09-29 22:31:21 -0700

    Update is_subsequence.py

--- Leetcode/is_subsequence.py
+++ Leetcode/is_subsequence.py
@@ -3,18 +3,25 @@
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        """
+        This function checks if string s is a subsequence of string t.
+        :param s: string
+        :param t: string
+        :return: bool
+        """
         i = j = 0
         while i < len(s) and j < len(t):
             if s[i] == t[j]:
                 i += 1
             j += 1
         return i == len(s)
-      
+
+# Driver code
 s = "abc"
 t = "ahbgdc"
 solution = Solution()
 print(solution.isSubsequence(s, t))  # Output: True
-
 s = "axc"
 t = "ahbgdc"
 print(solution.isSubsequence(s, t))  # Output: False
+
