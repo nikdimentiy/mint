427eaf55c7575cd12d830b1a1b68a893659c2dd8
nikdimentiy <nikdimentiy@yahoo.com>
2023-10-05 23:12:44 -0700

    Update main.py

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,42 +1,59 @@
-# old classic guessing game for two or more players (implementation in Python code--> game with computer)
-
 import random
 from hangman_art import stages, logo
 from hangman_words import word_list
 
+def play_hangman():
+    """
+    This function allows two or more players to play the classic hangman game with the computer.
+    The computer selects a random word from a predefined list, and the player(s) attempt to guess the word
+    by inputting one letter at a time. Players have a limited number of lives, and they win if they guess
+    the word correctly before running out of lives or lose if they exhaust all their lives.
 
-print(logo)
-game_is_finished = False
-lives = len(stages) - 1
+    The game uses ASCII art for displaying hangman stages and provides feedback to the players as they play.
 
-chosen_word = random.choice(word_list)
-word_length = len(chosen_word)
+    Args:
+        None
 
-display = []
-for _ in range(word_length):
-    display += "_"
+    Returns:
+        None
+    """
 
-while not game_is_finished:
-    guess = input("Guess a letter: ").lower()
+    print(logo)
+    game_is_finished = False
+    lives = len(stages) - 1
 
-    if guess in display:
-        print(f"You've already guessed {guess}")
+    chosen_word = random.choice(word_list)
+    word_length = len(chosen_word)
 
-    for position in range(word_length):
-        letter = chosen_word[position]
-        if letter == guess:
-            display[position] = letter
-    print(f"{' '.join(display)}")
+    display = []
+    for _ in range(word_length):
+        display += "_"
 
-    if guess not in chosen_word:
-        print(f"You guessed {guess}, that's not in the word. You lose a life.")
-        lives -= 1
-        if lives == 0:
+    while not game_is_finished:
+        guess = input("Guess a letter: ").lower()
+
+        if guess in display:
+            print(f"You've already guessed {guess}")
+
+        for position in range(word_length):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = letter
+        print(f"{' '.join(display)}")
+
+        if guess not in chosen_word:
+            print(f"You guessed {guess}, that's not in the word. You lose a life.")
+            lives -= 1
+            if lives == 0:
+                game_is_finished = True
+                print("You lose.")
+
+        if "_" not in display:
             game_is_finished = True
-            print("You lose.")
+            print("You win.")
 
-    if not "_" in display:
-        game_is_finished = True
-        print("You win.")
+        print(stages[lives])
 
-    print(stages[lives])
+# To start the game, you can call the play_hangman() function.
+if __name__ == "__main__":
+    play_hangman()
