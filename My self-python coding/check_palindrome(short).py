eafc95e098c46e625af8694c67efc300ad8b375d
nikdimentiy <nikdimentiy@yahoo.com>
2023-10-07 23:31:01 -0700

    Update check_palindrome(short).py

--- My self-python coding/check_palindrome(short).py
+++ My self-python coding/check_palindrome(short).py
@@ -1,9 +1,34 @@
-# the code analyzes whether the entered value is a palindrome (short version)
+import string
 
-string_1 = input("Enter the needed value: ")
-reverse_string = string_1[::-1]
+def is_palindrome(s):
+    """
+    Check if a given string is a palindrome (reads the same forwards and backwards).
 
-if string_1 == reverse_string:
-    print("YES - entered value is palindrome!")
-else:
-    print("NO - this value in not palindrome!")
+    Args:
+        s (str): The input string to be checked for palindrome status.
+
+    Returns:
+        bool: True if the input string is a palindrome, False otherwise.
+    """
+    # Remove spaces and punctuation and convert to lowercase
+    s = ''.join(char for char in s if char.isalnum()).lower()
+    
+    reverse_s = s[::-1]
+    
+    if s == reverse_s:
+        return True
+    else:
+        return False
+
+# Input validation loop
+while True:
+    string_1 = input("Enter a value (or 'exit' to quit): ")
+    
+    if string_1.lower() == 'exit':
+        break
+    
+    if is_palindrome(string_1):
+        print("YES - entered value is a palindrome!")
+    else:
+        print("NO - this value is not a palindrome!")
+
