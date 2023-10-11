c32c6b0b1ed5b37222b9ea9273c000c265ce5dee
nikdimentiy <nikdimentiy@yahoo.com>
2023-10-10 20:59:17 -0700

    Update hello.py

--- Test/hello.py
+++ Test/hello.py
@@ -1,11 +1,11 @@
+# Define a function to get the user's name and call the 'hello' function
 def main():
-    name = input("What is your name: ?")
-    hello(name)
+    name = input("What is your name: ?")  # Prompt the user for their name
+    print(hello(name))  # Call the 'hello' function and print the result
 
-
+# Define a function to greet a person with their name or default to "world"
 def hello(name="world"):
-    return f"Hello, {name}"
+    return f"Hello, {name}"  # Return a greeting message with the provided name
 
-
-if __name__ == "__name__":
-    main()
+if __name__ == "__main__":  # Check if the script is being run as the main program
+    main()  # Call the 'main' function if this script is the main program
