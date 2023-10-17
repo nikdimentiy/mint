8afdc51ccd0dc0ce87991c1993febfbe8dfac9d2
nikdimentiy <nikdimentiy@yahoo.com>
2023-10-16 23:18:55 -0700

    Update main.py
    create doc string and add comments

--- Auction/main.py
+++ Auction/main.py
@@ -1,14 +1,26 @@
+# Import necessary modules
 from replit import clear
 from art import logo
+
+# Display the program logo
 print(logo)
 
+# Initialize an empty dictionary to store bids and a flag for bidding completion
 bids = {}
 bidding_finished = False
 
 def find_highest_bidder(bidding_record):
+  """
+  Find the highest bidder and the corresponding bid amount.
+
+  Args:
+    bidding_record (dict): A dictionary containing bidder names as keys and their respective bid amounts as values.
+
+  Prints:
+    The winner and their winning bid.
+  """
   highest_bid = 0
   winner = ""
-  # bidding_record = {"Angela": 123, "James": 321}
   for bidder in bidding_record:
     bid_amount = bidding_record[bidder]
     if bid_amount > highest_bid:
@@ -16,13 +28,21 @@ def find_highest_bidder(bidding_record):
       winner = bidder
   print(f"The winner is {winner} with a bid of ${highest_bid}")
 
+# Main bidding loop
 while not bidding_finished:
   name = input("What is your name?: ")
   price = int(input("What is your bid?: $"))
   bids[name] = price
-  should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+  should_continue = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+  
   if should_continue == "no":
     bidding_finished = True
     find_highest_bidder(bids)
   elif should_continue == "yes":
     clear()
+
+# Driver code
+# The program starts with an empty dictionary for bids and asks for user input.
+# Users can enter their name and bid amount.
+# The bidding continues until the user decides to stop.
+# Once bidding is finished, the program finds and displays the highest bidder and their bid amount.
