0903b7b091b9a79bc9d326a72b8d05ce45fcc24d
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-10-24 21:26:36 -0700

    Update average_sequence_of_elements.cs

--- CSharp/average_sequence_of_elements.cs
+++ CSharp/average_sequence_of_elements.cs
@@ -1,32 +1,36 @@
-// the code calculate: the arithmetic mean the elements of sequence
-
 using System;
 
 namespace Softcode
 {
     class MainClass
     {
+        /// <summary>
+        /// Main entry point of the program. Calculates the arithmetic mean of a sequence of integers.
+        /// </summary>
+        /// <param name="args">Command-line arguments (not used in this program).</param>
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter integer number (make a sequence): ");
-            Console.WriteLine("For the terminate input - please enter (zero)");
-            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter integer numbers to create a sequence:");
+            Console.WriteLine("To terminate input, please enter (zero).");
 
+            int n = Convert.ToInt32(Console.ReadLine());
             double sum = 0;
             int count = 0;
 
+            // Continue to input numbers until the user enters 0.
             while (n != 0)
             {
-                sum = sum + n;
-                n = Convert.ToInt32(Console.ReadLine());
-                count++;
+                sum += n; // Add the entered number to the sum.
+                n = Convert.ToInt32(Console.ReadLine()); // Prompt for the next number.
+                count++; // Increase the count of entered numbers.
             }
-            
+
+            // Calculate the arithmetic mean (average) of the sequence.
             double result = sum / count;
+
             Console.WriteLine();
-            Console.Write("The arithmetic mean of elements of the sequence is: ");
+            Console.Write("The arithmetic mean of the elements of the sequence is: ");
             Console.WriteLine(result);
         }
     }
 }
-
