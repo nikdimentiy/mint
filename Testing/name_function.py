30b2688d52d67feb3398e3a796602e9f9735c844
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-11-03 18:51:20 -0700

    Update name_function.py

--- Testing/name_function.py
+++ Testing/name_function.py
@@ -1,5 +1,14 @@
 def get_formatted_name(first, last):
-    """This function build formated full name"""
-    full_name = first + " " + last
-
-    return full_name.title()
+    """Build a formatted full name."""
+    try:
+        # Check if first or last name is empty or contains only whitespace characters.
+        if not first.strip() or not last.strip():
+            raise ValueError("Both first name and last name must be provided.")
+        
+        # Format the full name and return it.
+        full_name = first.strip() + " " + last.strip()
+        return full_name.title()
+    except ValueError as ve:
+        return str(ve)
+    except Exception as e:
+        return f"An error occurred: {e}"
