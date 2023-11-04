da4e15fffcedeed6e5c6a374430b6cfe0f657e7d
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-11-03 18:52:01 -0700

    Update test_name_function.py

--- Testing/test_name_function.py
+++ Testing/test_name_function.py
@@ -1,14 +1,13 @@
 import unittest
 from name_function import get_formatted_name
 
-
 class NameTestCase(unittest.TestCase):
-    """Tests for function -> (name_function)"""
+    """Tests for function (name_function)"""
 
     def test_first_last_name(self):
         """names like: Janis and Joplin -> working right?"""
-        formated_name = get_formatted_name('janis', 'joplin')
-        self.assertEqual(formated_name, "Janis Joplin")
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, "Janis Joplin")
 
-
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
