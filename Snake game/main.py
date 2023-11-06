9e1a89b41b7971fb7fbacd2511244f4546e076e9
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-11-05 19:08:42 -0800

    Update main.py

--- Snake game/main.py
+++ Snake game/main.py
@@ -4,16 +4,19 @@ from food import Food
 from scoreboard import Scoreboard
 import time
 
+# Set up the game window
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-screen.tracer(0)
+screen.tracer(0)  # Turn off automatic screen updates
 
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
+# Create game objects
+snake = Snake()  # Initialize the snake
+food = Food()    # Initialize the food
+scoreboard = Scoreboard()  # Initialize the scoreboard
 
+# Listen for keyboard inputs to control the snake
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -22,31 +25,31 @@ screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+    screen.update()  # Manually update the screen to reduce flickering
+    time.sleep(0.1)  # Control the game speed
 
-    #Detect collision with food.
+    snake.move()  # Move the snake
+
+    # Detect collision with food
     if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
+        food.refresh()  # Generate a new food position
+        snake.extend()  # Extend the snake
+        scoreboard.increase_score()  # Increase the player's score
 
-    #Detect collision with wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    # Detect collision with the wall
+    if (
+        snake.head.xcor() > 280
+        or snake.head.xcor() < -280
+        or snake.head.ycor() > 280
+        or snake.head.ycor() < -280
+    ):
         game_is_on = False
         scoreboard.game_over()
 
-    #Detect collision with tail.
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    # Detect collision with the snake's own tail
+    for segment in snake.segments[1:]:  # Skip the head when checking for self-collision
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
-
-
-
-
 
 screen.exitonclick()
