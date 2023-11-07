ccaf65ebe34dd58722ba92f3fe70ac99a9137c2f
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-11-06 18:41:27 -0800

    Update twitter.py

--- RegEx/twitter.py
+++ RegEx/twitter.py
@@ -1,9 +1,16 @@
-# Ignores query and fragment
+import re  # Import the regular expression module to perform pattern matching.
 
-import re
-
+# Prompt the user to input a URL and remove leading/trailing whitespaces.
 url = input("URL: ").strip()
 
+# Use a regular expression to search for a specific pattern in the input URL:
+# - Starts with "http://" or "https://"
+# - Followed by an optional "www."
+# - Followed by "twitter.com/"
+# - Captures the username (a sequence of lowercase letters, digits, and underscores)
+# - Performs a case-insensitive search
 matches = re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE)
+
 if matches:
+    # If a match is found, print the captured username (group 1).
     print("Username:", matches.group(1))
