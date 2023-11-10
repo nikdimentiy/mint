c209769fb13171a211b20e5bf70aab8ff6f4e36c
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-11-09 19:46:18 -0800

    Update iter_loop.py

--- Silicon Rockstar/iter_loop.py
+++ Silicon Rockstar/iter_loop.py
@@ -1,9 +1,24 @@
 class DoubleElementListIterator:
     def __init__(self, lst):
+        """
+        Initializes the iterator with a list and sets the starting index to 0.
+
+        Args:
+        - lst: The list to iterate over.
+        """
         self.lst = lst
         self.i = 0
 
     def __next__(self):
+        """
+        Returns the next pair of elements in the list.
+
+        Returns:
+        - A tuple containing the current and next element in the list.
+
+        Raises:
+        - StopIteration: If there are no more pairs of elements in the list.
+        """
         if self.i < len(self.lst):
             result = (self.lst[self.i - 2], self.lst[self.i - 1])
             self.i += 2
@@ -13,9 +28,10 @@ class DoubleElementListIterator:
 
 class MyList(list):
     def __iter__(self):
-        return DoubleElementListIterator(self)
+        """
+        Returns an instance of DoubleElementListIterator to iterate over the list.
 
-# Example usage
-my_list = MyList([1, 2, 3, 4, 5, 6])
-for pair in my_list:
-    print(pair)
+        Returns:
+        - An iterator object for iterating over the list.
+        """
+        return DoubleElementListIterator(self)
