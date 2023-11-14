0850727edf56eea9b77dc41e63d342956c7929b3
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-11-13 17:24:31 -0800

    Update player.py

--- Turtle Crossing Road/player.py
+++ Turtle Crossing Road/player.py
@@ -6,22 +6,27 @@ FINISH_LINE_Y = 280
 
 
 class Player(Turtle):
+    """A class to represent the player in a game"""
 
     def __init__(self):
-        super().__init__()
-        self.shape("turtle")
-        self.penup()
-        self.go_to_start()
-        self.setheading(90)
+        """Initialize the player"""
+        super().__init__()  # Call the constructor of the parent class
+        self.shape("turtle")  # Set the shape of the player to a turtle
+        self.penup()  # Don't draw lines when moving
+        self.go_to_start()  # Move the player to the starting position
+        self.setheading(90)  # Set the initial heading of the player to face upwards
 
     def go_up(self):
-        self.forward(MOVE_DISTANCE)
+        """Move the player upwards by the move distance"""
+        self.forward(MOVE_DISTANCE)  # Move the player forward by the move distance
 
     def go_to_start(self):
-        self.goto(STARTING_POSITION)
+        """Move the player to the starting position"""
+        self.goto(STARTING_POSITION)  # Move the player to the starting position
 
     def is_at_finish_line(self):
-        if self.ycor() > FINISH_LINE_Y:
-            return True
+        """Check if the player has reached the finish line"""
+        if self.ycor() > FINISH_LINE_Y:  # If the player's y-coordinate is greater than the finish line y-coordinate
+            return True  # The player has reached the finish line
         else:
-            return False
+            return False  # The player has not reached the finish line
