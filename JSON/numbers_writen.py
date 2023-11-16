91e118ba8176696ff6d1994060fb0e9ca5d5f10d
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-11-15 22:06:01 -0800

    Update numbers_writen.py

--- JSON/numbers_writen.py
+++ JSON/numbers_writen.py
@@ -1,7 +1,11 @@
 import json
 
 filename = 'numbers.json'
-with open(filename) as f_obj:
-	numbers = json.load(f_obj)
 
+# Open the JSON file in read mode using a with statement
+with open(filename) as f_obj:
+    # Load the JSON data from the file into a Python data structure
+    numbers = json.load(f_obj)
+
+# Print the loaded data
 print(numbers)
