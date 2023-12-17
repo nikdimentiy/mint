11c30be1c574c5915f30fa8f9e23f6e825287b7f
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-12-17 14:12:34 -0800

    Update file_converter_size.py
    feat: Add user input, unit conversion, and exception handling for file size
    
    This commit enhances the existing code by incorporating user input for file size and its unit (KB, MB, GB, or TB). The program now performs unit conversion, displays the result in gigabytes, and gracefully handles exceptions, providing informative error messages for invalid inputs.

--- Silicon Rockstar/file_converter_size.py
+++ Silicon Rockstar/file_converter_size.py
@@ -3,11 +3,6 @@ KB = 1 << (10 * 1)  # 1KB = 1024 bytes
 MB = 1 << (10 * 2)  # 1MB = 1024 KB
 GB = 1 << (10 * 3)  # 1GB = 1024 MB
 TB = 1 << (10 * 4)  # 1TB = 1024 GB
-PB = 1 << (10 * 5)  # 1PB = 1024 TB
-EB = 1 << (10 * 6)  # 1EB = 1024 PB
-ZB = 1 << (10 * 7)  # 1ZB = 1024 EB
-YB = 1 << (10 * 8)  # 1YB = 1024 ZB
-
 
 def convert_file_size(file_size, unit):
     """
@@ -20,26 +15,35 @@ def convert_file_size(file_size, unit):
     Returns:
         str: A formatted string representing the file size in gigabytes.
     """
-    # Convert file size to bytes based on unit
-    if unit == "KB":
-        file_size *= KB
-    elif unit == "MB":
-        file_size *= MB
-    elif unit == "GB":
-        file_size *= GB
-    elif unit == "TB":
-        file_size *= TB
-    else:
-        return "Invalid unit. Please enter KB, MB, GB, or TB."
+    try:
+        # Convert file size to bytes based on unit
+        if unit == "KB":
+            file_size *= KB
+        elif unit == "MB":
+            file_size *= MB
+        elif unit == "GB":
+            file_size *= GB
+        elif unit == "TB":
+            file_size *= TB
+        else:
+            raise ValueError("Invalid unit. Please enter KB, MB, GB, or TB.")
 
-    # Return the result in gigabytes
-    return f"File size: {file_size / GB:.2f}GB"
+        # Return the result in gigabytes
+        return f"File size: {file_size / GB:.2f}GB"
+    except ValueError as e:
+        return str(e)
 
+try:
+    # Get user input for file size and unit
+    file_size = float(input("Enter file size: "))
+    unit = input("Enter unit (KB, MB, GB, TB): ").upper()  # Convert to uppercase
 
-# Get user input for file size and unit
-file_size = float(input("Enter file size: "))
-unit = input("Enter unit (KB, MB, GB, TB): ").upper()  # Convert to uppercase
+    # Display the result
+    result = convert_file_size(file_size, unit)
+    print(result)
 
-# Display the result
-result = convert_file_size(file_size, unit)
-print(result)
+except ValueError as e:
+    print(f"Error: {e}. Please enter a valid numeric file size.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
