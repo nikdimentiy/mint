c120b1e468738aa64b1c59b866a2a2d4f7b56aad
Dmytro Nikey <nikdimentiy@yahoo.com>
2023-12-30 23:20:40 -0800

    Update question_model.py

--- Quiz Game/question_model.py
+++ Quiz Game/question_model.py
@@ -1,5 +1,22 @@
 class Question:
+    """
+    A class representing a question in a quiz.
+
+    Attributes:
+    - text (str): The text of the question.
+    - answer (str): The correct answer to the question.
+
+    Methods:
+    - None
+    """
 
     def __init__(self, q_text, q_answer):
+        """
+        Initializes a Question instance.
+
+        Parameters:
+        - q_text (str): The text of the question.
+        - q_answer (str): The correct answer to the question.
+        """
         self.text = q_text
         self.answer = q_answer
