13537146678748ec56d6224b1adfe6b7d437407e
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-05-17 23:39:48 -0700

    Added comments and docstring, improved readability and variable naming in triangle area calculation program

--- CS50/area_of_triangle.c
+++ CS50/area_of_triangle.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(int argc, char *argv[]) {
-    double first_k, second_k, alpha_angle, area, alpha_degrees, pi = 3.141593;
+/**
+ * This program calculates the area of a triangle given the lengths of two sides (catheti)
+ * and the angle between them in degrees. The formula used is:
+ * 
+ * Area = 0.5 * a * b * sin(C)
+ * 
+ * where:
+ * a and b are the lengths of the two sides,
+ * C is the angle between them.
+ * 
+ * The program converts the angle from degrees to radians before using the sine function.
+ */
 
+int main(int argc, char *argv[]) {
+    double first_k, second_k, alpha_angle, area, alpha_radians;
+    const double pi = 3.141593; // Define a constant for Pi
+
+    // Prompt the user to enter the length of the first cathetus
     printf("Enter the length of the first cathetus: \n");
     scanf("%lf", &first_k);
 
-    printf("Enter the lenght of the second cathetus: \n");
+    // Prompt the user to enter the length of the second cathetus
+    printf("Enter the length of the second cathetus: \n");
     scanf("%lf", &second_k);
 
-    printf("Enter angle value: \n");
+    // Prompt the user to enter the angle in degrees
+    printf("Enter the angle value in degrees: \n");
     scanf("%lf", &alpha_angle);
 
-    alpha_degrees = alpha_angle * pi / 180;
+    // Convert the angle from degrees to radians
+    alpha_radians = alpha_angle * pi / 180;
 
-    area = ((first_k * second_k) * (sin)(alpha_degrees));
-    printf("The area of the triangle is: %3.2lf", area / 2);
+    // Calculate the area of the triangle
+    area = 0.5 * first_k * second_k * sin(alpha_radians);
+    printf("The area of the triangle is: %.2lf\n", area);
 
     return 0;
 }
