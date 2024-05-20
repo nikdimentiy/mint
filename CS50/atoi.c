2ce2ce31060a53dacad7fe6ed67d6bd8cda50e8c
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-05-19 21:54:18 -0700

    Update atoi.c

--- CS50/atoi.c
+++ CS50/atoi.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * This program takes a number as input from the user and prints it.
+ */
+
 int main(int argc, char const *argv[]) {
   char input[10];
   int num;
 
   printf("Enter a number, please: \n");
-  fgets(input, 10, stdin);
-  num = atoi(input);
+  fgets(input, 10, stdin); // Read user input as a string
+  num = atoi(input); // Convert the string to an integer
   printf("You entered number: %d\n", num);
 
   return 0;
