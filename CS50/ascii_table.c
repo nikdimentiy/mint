42e771f0fe7a39127984141e5ec9bea478aae469
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-05-19 21:53:10 -0700

    Update ascii_table.c

--- CS50/ascii_table.c
+++ CS50/ascii_table.c
@@ -1,27 +1,27 @@
-// The program show: uppercase and lowercase letter in ASCII table
-// American Standard Code for Information Interchange, is a character encoding 
-// standard for electronic communication. ASCII codes represent text in computers, 
-// telecommunications equipment, and other devices. Most modern character-encoding 
-// schemes are based on ASCII, although they support many additional characters
-
 #include <stdio.h>
 
+/**
+ * This program displays uppercase and lowercase letters in the ASCII table.
+ * ASCII (American Standard Code for Information Interchange) is a character encoding
+ * standard used for electronic communication. It represents text in computers,
+ * telecommunications equipment, and other devices.
+ */
+
 int main(void)
-
 {
-	for (int i = 65; i < 65 + 26; i++)
-	{
-		printf("%c: %i\n", (char) i, i);
-	}
+    // Display uppercase letters
+    for (int i = 65; i < 65 + 26; i++)
+    {
+        printf("%c: %i\n", (char) i, i);
+    }
 
+    printf("\n");
 
-	printf("\n");
+    // Display lowercase letters
+    for (int i = 97; i < 97 + 26; i++)
+    {
+        printf("%c: %i\n", (char) i, i);
+    }
 
-
-	for (int i = 97; i < 97 + 26; i++)
-	{
-		printf("%c: %i\n", (char) i, i);
-	}
-	
-	return 0;
+    return 0;
 }
