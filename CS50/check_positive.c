da0d1b72e4916a31f66f22fd01a901f796b2cf7a
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-05-20 23:46:10 -0700

    Added comments, docstring, and improved user input validation.

--- CS50/check_positive.c
+++ CS50/check_positive.c
@@ -1,28 +1,34 @@
-//checking for a positive number
-
 #include <stdio.h>
 
-//prototype
+/*
+    This program prompts the user to input a positive integer and then prints the entered number.
+    It demonstrates a simple function to ensure that the user inputs a positive integer.
+*/
 
+// Function prototype
 int GetPositiveInt();
 
 int main(void)
 {
+    // Call GetPositiveInt() function to get a positive integer from the user
     int n = GetPositiveInt();
+    
+    // Print the entered positive integer as a confirmation
     printf("Thanks for the %i!\n", n);
 }
 
-// get a positive integer from a user
-
+// Function to get a positive integer from the user
 int GetPositiveInt(void)
 {
     int n;
     do
     {
-        printf("Please give me a positive integer number:");
+        // Prompt the user to input a positive integer
+        printf("Please give me a positive integer number: ");
         scanf("%i", &n);
     }
-
+    // Continue prompting until a positive integer is entered
     while (n < 1);
-    return n;
+    
+    return n; // Return the positive integer entered by the user
 }
