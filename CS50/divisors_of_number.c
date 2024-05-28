22372f635196e3d5ee09541b238d602471d9528e
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-05-27 20:27:59 -0700

    Update divisors_of_number.c

--- CS50/divisors_of_number.c
+++ CS50/divisors_of_number.c
@@ -1,22 +1,34 @@
-// finding divisors of an entered number
-
 #include <stdio.h>
 
+/**
+ * @brief Main function to find and print the divisors of an entered integer.
+ * 
+ * This program prompts the user to enter an integer. It then calculates and
+ * prints all the divisors of the entered number. Finally, it prints the total
+ * count of these divisors.
+ * 
+ * @return int Returns 0 on successful completion.
+ */
 int main(void) {
     int n, count;
-    printf("Give me integer number: \n");
+
+    // Prompt the user to enter an integer
+    printf("Give me an integer number: \n");
     scanf("%d", &n);
 
     count = 0;
 
+    // Loop to find and print all divisors of n
     for (int i = 1; i <= n; i++) {
         if (n % i == 0) {
+            // If i is a divisor, print it and increment the count
             printf("Divisor of entered number = %d\n", i);
             count++;
         }
     }
-    printf("Total divisors of entered number = ");
-    printf("%d\n", count);
+
+    // Print the total number of divisors
+    printf("Total divisors of entered number = %d\n", count);
 
     return 0;
 }
