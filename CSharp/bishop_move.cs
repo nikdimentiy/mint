a2668f292b8b77c38edcd546b6b09aa6e9aa4287
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-05-31 23:21:07 -0700

    Add bishop attack check program with input validation and comments

--- CSharp/bishop_move.cs
+++ CSharp/bishop_move.cs
@@ -1,37 +1,54 @@
-// The program calculates: whether a bishop (in chess) will be able to attack a given piece
-
 using System;
 
 namespace ConsoleApplication1
 {
     internal class Program
     {
+        /// <summary>
+        /// The entry point of the program which determines if a bishop can attack a given piece in chess.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
         public static void Main(string[] args)
         {
+            // Prompt the user to enter the bishop's coordinates
             Console.WriteLine("Enter bishop coordinates");
+
+            // Declare variables for the bishop's row and column
             int row1, column1;
 
+            // Prompt and read the row of the bishop from the user
             Console.WriteLine("Enter row of the bishop:");
             row1 = Convert.ToInt32(Console.ReadLine());
 
+            // Prompt and read the column of the bishop from the user
             Console.WriteLine("Enter column of the bishop:");
             column1 = Convert.ToInt32(Console.ReadLine());
 
+            // Prompt the user to enter the coordinates of the chess piece
             Console.WriteLine("Enter coordinates of the chess piece");
+
+            // Declare variables for the piece's row and column
             int row2, column2;
 
+            // Prompt and read the row of the chess piece from the user
             Console.WriteLine("Enter row of the chess piece:");
             row2 = Convert.ToInt32(Console.ReadLine());
 
+            // Prompt and read the column of the chess piece from the user
             Console.WriteLine("Enter column of the chess piece:");
             column2 = Convert.ToInt32(Console.ReadLine());
 
+            // Check if the bishop can attack the given piece
+            // A bishop attacks diagonally, which means the absolute difference between
+            // the rows and columns of the two pieces should be the same
             if (Math.Abs(row1 - row2) == Math.Abs(column1 - column2))
             {
-                Console.WriteLine("YES"); 
+                // If the condition is met, the bishop can attack the piece
+                Console.WriteLine("YES");
             }
             else
             {
+                // If the condition is not met, the bishop cannot attack the piece
                 Console.WriteLine("NO");
             }
         }
