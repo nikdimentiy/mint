a254b00d2a90b4d8b4618c0c7e8dc847ec7ff19e
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-06-03 22:38:19 -0700

    Update factorization_array.c

--- CS50/factorization_array.c
+++ CS50/factorization_array.c
@@ -1,35 +1,47 @@
 #include <stdio.h>
 
-// returns number of multipliers
-// buffer A should be enough for x factors
-int get_number_factors(int x, int A[])
-{
-    int top = 0;
-    int divisor = 2;
-    while (x != 1)
-    {
-        while (x%divisor == 0)
-        {
-            A[top] = divisor;
-            top += 1;
-            x /= divisor;
+/**
+ * Function to get the prime factors of a given number.
+ *
+ * This function finds the prime factors of the given integer x and stores them in the provided array A.
+ * It returns the number of prime factors found.
+ *
+ * @param x The number to be factorized.
+ * @param A An array to store the prime factors. The caller must ensure this is large enough.
+ * @return The number of prime factors stored in A.
+ */
+int get_number_factors(int x, int A[]) {
+    int top = 0;         // Index to keep track of the position in the array A
+    int divisor = 2;     // The current divisor to check for factorization
+
+    // Loop until x is reduced to 1
+    while (x != 1) {
+        // Check if divisor is a factor of x
+        while (x % divisor == 0) {
+            A[top] = divisor;  // Store the divisor in the array A
+            top += 1;          // Move to the next position in the array
+            x /= divisor;      // Divide x by the divisor
         }
-        divisor += 1;
+        divisor += 1;          // Move to the next potential divisor
     }
-    return top;
+    return top;  // Return the number of factors found
 }
 
-int main(int argc, char* argv[])
-{
-    int x;
-    printf("Enter number to factorize:");
+int main(int argc, char* argv[]) {
+    int x;  // Variable to store the input number
+    printf("Enter number to factorize: ");
     scanf("%d", &x);
-    int A[100];
-    int N;
+
+    int A[100];  // Array to store the prime factors (assuming a max of 100 factors)
+    int N;       // Variable to store the number of factors found
+
+    // Get the prime factors of x
     N = get_number_factors(x, A);
 
-    for(int i = 0; i < N; ++i)
+    // Print the prime factors
+    for(int i = 0; i < N; ++i) {
         printf("%d ", A[i]);
+    }
     printf("\n");
 
     return 0;
