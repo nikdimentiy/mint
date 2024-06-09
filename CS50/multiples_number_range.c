0bc11298efbe428e70c6bc0973355c2576d36878
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-06-08 23:40:04 -0700

    Update multiples_number_range.c

--- CS50/multiples_number_range.c
+++ CS50/multiples_number_range.c
@@ -1,28 +1,43 @@
-// The program finds multiples of number in the range
 #include <stdio.h>
 
+/**
+ * @brief Finds and prints multiples of a given number in a specified range.
+ *
+ * This program prompts the user to enter a start range, end range, and a number
+ * to determine the multiplicity. It then finds and prints all numbers within
+ * the range that are multiples of the specified number and counts them.
+ *
+ * @return 0 on successful execution.
+ */
 int main(void) {
-    int x, y, z;
+    int start, end, multiple;
     int count = 0;
+
+    // Prompt the user for the start of the range.
     printf("Enter any positive integer number (start range): \n");
-    scanf("%i", &x);
+    scanf("%i", &start);
 
+    // Prompt the user for the end of the range.
     printf("Enter any positive integer number (finish range): \n");
-    scanf("%i", &y);
+    scanf("%i", &end);
 
+    // Prompt the user for the number that determines multiplicity.
     printf("Enter the number, that determines multiplicity: ");
-    scanf("%i", &z);
+    scanf("%i", &multiple);
 
     printf("\n");
 
-    for (int i = x; i <= y; i++) {
-        if (i % z == 0) {
+    // Iterate over the range and find multiples of the given number.
+    for (int i = start; i <= end; i++) {
+        if (i % multiple == 0) {
             printf("This number: %i - meets the specified conditions!\n", i);
             count++;
         }
     }
+
     printf("\n");
-    printf("Total of even numbers on a given segment is: %i\n", count);
+    // Print the total count of numbers that meet the conditions.
+    printf("Total of numbers meeting the specified conditions in the given segment is: %i\n", count);
 
     return 0;
 }
