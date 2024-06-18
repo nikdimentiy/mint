8de5e7ed7d7cadc3c1f64b402a50565177a18fff
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-06-17 23:31:27 -0700

    Added docstrings, comments, and code improvements for better readability and maintainability

--- CS50/samples_of_recursion.c
+++ CS50/samples_of_recursion.c
@@ -1,27 +1,50 @@
-// The code represents and calculates many recursion algorithms:
-// factorial, greatest common divisor, fast degree of the number,
-// and Fibonacci sequence (needed member - the number)
 #include <stdio.h>
 
+/**
+ * Computes the factorial of a given number n.
+ * 
+ * @param n The number to compute the factorial of.
+ * @return The factorial of n.
+ */
 int factorial(int n) {
-    if (0 == n)
+    if (n == 0)
         return 1;
     return factorial(n - 1) * n;
 }
 
+/**
+ * Computes the greatest common divisor (GCD) of two integers a and b.
+ * 
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The GCD of a and b.
+ */
 int gcd(int a, int b) {
-    if (0 == b) return a;
+    if (b == 0) return a;
     return gcd(b, a % b);
 }
 
+/**
+ * Computes the result of raising a number a to the power of n using fast exponentiation.
+ * 
+ * @param a The base number.
+ * @param n The exponent.
+ * @return The result of a raised to the power of n.
+ */
 double fast_degree(double a, int n) {
-    if (0 == n) return 1;
+    if (n == 0) return 1;
     if (n % 2 == 1)
         return a * fast_degree(a, n - 1);
     else
         return fast_degree(a * a, n / 2);
 }
 
+/**
+ * Computes the nth Fibonacci number.
+ * 
+ * @param n The position in the Fibonacci sequence.
+ * @return The nth Fibonacci number.
+ */
 int fib(int n) {
     if (n <= 1) return n;
     else return fib(n - 1) + fib(n - 2);
@@ -29,16 +52,20 @@ int fib(int n) {
 
 int main(void) {
     int n, m;
+
+    // Prompt the user for input
     printf("Enter 2 (two) integers number:\n");
-    printf("First integer - for calculate: factorial of entered number, fast degree and number of sequence Fibonacci: \n");
+    printf("First integer - for calculating: factorial, fast degree, and Fibonacci sequence number:\n");
     scanf("%d", &n);
-    printf("Second integer - for calculate: greatest common divisor: \n");
+    printf("Second integer - for calculating: greatest common divisor:\n");
     scanf("%d", &m);
+
+    // Print results of the calculations
     printf("\n");
     printf("The Factorial(%d) = %d\n", n, factorial(n));
     printf("Greatest common divisor(%d, %d) = %d\n", n, m, gcd(n, m));
-    printf("Fast degree(%d, %d) = %lf\n", n, m, fast_degree(n, m));
-    printf("Number(member) of sequence Fibonacci(%d) = %d\n", n, fib(n));
+    printf("Fast degree(%d, %d) = %lf\n", n, m, fast_degree((double)n, m));
+    printf("Number (member) of Fibonacci sequence(%d) = %d\n", n, fib(n));
 
     return 0;
 }
