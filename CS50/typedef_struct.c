7a13ca3e5c3167203ad116e67868b7acc905e1e5
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-06-25 22:14:55 -0700

    Added documentation header and improved code readability in main function

--- CS50/typedef_struct.c
+++ CS50/typedef_struct.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct
-{
-    int id;
-    char title[40];
-    float hours;
+// Define a structure for a course
+typedef struct {
+    int id;                 // Course ID
+    char title[40];         // Title of the course (up to 39 characters)
+    float hours;            // Duration of the course in hours
 } course;
 
-int main()
-{
-    course cs1;
-    course cs2;
+int main() {
+    course cs1;             // Declare variable for first course
+    course cs2;             // Declare variable for second course
 
+    // Initialize details for cs1
     cs1.id = 123456;
     strcpy(cs1.title, "JavaScript Basics");
     cs1.hours = 12.30;
 
-    /* initialize cs2 */
+    // Initialize details for cs2
     cs2.id = 341281;
     strcpy(cs2.title, "Advanced C++");
     cs2.hours = 14.25;
 
-    /* display course info */
-    printf("%d\t%s\t%4.2f\n", cs1.id, cs1.title, cs1.hours);
-    printf("%d\t%s\t%4.2f\n", cs2.id, cs2.title, cs2.hours);
+    // Display course information
+    printf("Course ID\tTitle\t\t\t\tHours\n");
+    printf("%d\t\t%s\t\t%4.2f\n", cs1.id, cs1.title, cs1.hours);
+    printf("%d\t\t%s\t\t%4.2f\n", cs2.id, cs2.title, cs2.hours);
 
     return 0;
 }
