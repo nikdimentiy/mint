6f40a6b87d1fb020703623f6d6a13825cc9c54e0
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-07-06 23:59:06 -0700

    Update str_copy.c

--- CS50/str_copy.c
+++ CS50/str_copy.c
@@ -1,24 +1,38 @@
+/**
+ * This program concatenates two strings without using the strcat() function.
+ * The first string is "programming is " and the second string is "awesome".
+ * The program appends the second string to the first string and then prints the result.
+ */
+
 #include <stdio.h>
 
 int main()
 {
-	char str_1[] = "programming is ";
-	char str_2[] = "awesome";
+    // Declare and initialize two strings
+    char str_1[] = "programming is ";
+    char str_2[] = "awesome";
 
-	int length, j;
+    // Variable to keep track of the length of str_1
+    int length = 0;
+    int j;
 
-	length = 0;
-	while (str_1[length] != '\0')
-	{
-		length++;
-	}
+    // Calculate the length of str_1
+    while (str_1[length] != '\0')
+    {
+        length++;
+    }
 
-	for (j = 0; str_2[j] != '\0'; j++, length++)
-	{
-		str_1[length] = str_2[j];
-	}
+    // Append str_2 to str_1
+    for (j = 0; str_2[j] != '\0'; j++, length++)
+    {
+        str_1[length] = str_2[j];
+    }
 
-	str_1[length] = '\0';
-	printf("After concatenation --> the string is: %s\n", str_1);
-	return 0;
+    // Null-terminate the resulting string
+    str_1[length] = '\0';
+
+    // Print the concatenated string
+    printf("After concatenation --> the string is: %s\n", str_1);
+
+    return 0;
 }
