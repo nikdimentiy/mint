dd999da99c7814123b18abfe6628ac426908540d
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-07-12 23:41:49 -0700

    The provided C code defines a program that generates and prints the first 5 even numbers.

--- CS50/return_pointer_function.c
+++ CS50/return_pointer_function.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 
-int * get_evens();
+// Function prototype for get_evens
+int* get_evens();
 
 int main() {
     int *a;
     int k;
 
-    a = get_evens(); /* get first 5 even numbers */
+    // Call get_evens function to get the first 5 even numbers
+    a = get_evens();
+    
+    // Print the first 5 even numbers
     for (k = 0; k < 5; k++)
-      printf("%d\n", a[k]); 
+        printf("%d\n", a[k]); 
 
     return 0;
 }
 
-int * get_evens () {
-   static int nums[5];
-   int k;
-   int even = 0;
+// Function to generate the first 5 even numbers
+int* get_evens() {
+    static int nums[5]; // Static array to store the even numbers
+    int k;
+    int even = 0;
 
-   for (k = 0; k < 5; k++) {
+    // Generate and store the first 5 even numbers in the array
+    for (k = 0; k < 5; k++) {
         nums[k] = even += 2;
-   }
+    }
 
-    return (nums);
+    return nums; // Return the array of even numbers
 }
