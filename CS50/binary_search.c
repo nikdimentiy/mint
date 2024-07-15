fd70f6e0616ab597051a9c6672d98f1809dc49ae
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-07-14 21:40:07 -0700

    This C program implements the binary search algorithm to find the position of a target value within a sorted array of integers.

--- CS50/binary_search.c
+++ CS50/binary_search.c
@@ -1,49 +1,56 @@
-/* binary search - is a search algorithm that finds the position of a target value 
-   within a sorted array. Binary search compares the target value to the middle element of the array. 
-   If they are not equal, the half in which the target cannot lie is eliminated and the search 
-   continues on the remaining half, again taking the middle element to compare to the target value,
-   and repeating this until the target value is found. If the search ends with the remaining half being empty,
-   the target is not in the array */
-
-// binary search:  divide and conquer
-
 #include <stdio.h>
 
-int main()
-{
-  int c, first, last, middle, n, search, array[100];
+/**
+ * @brief Performs binary search on a sorted array.
+ *
+ * This program takes an array of integers and a target value as input. It uses the binary search
+ * algorithm to find the target value in the array. If the target is found, it prints the position
+ * of the target value. If the target is not found, it indicates that the target is not present in
+ * the array.
+ *
+ * @return int Always returns 0 to indicate successful execution.
+ */
 
-  printf("Enter number of elements\n");
-  scanf("%d", &n);
+int main() {
+    int c, first, last, middle, n, search, array[100];
 
-  printf("Enter %d integers\n", n);
+    // Prompt the user to enter the number of elements in the array
+    printf("Enter number of elements\n");
+    scanf("%d", &n);
 
-  for (c = 0; c < n; c++)
-    scanf("%d", &array[c]);
+    // Prompt the user to enter the elements of the array
+    printf("Enter %d integers\n", n);
+    for (c = 0; c < n; c++)
+        scanf("%d", &array[c]);
 
-  printf("Enter value to find\n");
-  scanf("%d", &search);
+    // Prompt the user to enter the value to search for
+    printf("Enter value to find\n");
+    scanf("%d", &search);
 
-  first = 0;
-  last = n - 1;
-  middle = (first + last) / 2;
-
-  while (first <= last)
-  {
-    if (array[middle] < search)
-      first = middle + 1;
-    else if (array[middle] == search)
-    {
-      printf("%d found at location %d.\n", search, middle + 1);
-      break;
-    }
-    else
-      last = middle - 1;
-
+    first = 0;
+    last = n - 1;
     middle = (first + last) / 2;
-  }
-  if (first > last)
-    printf("Not found! %d isn't present in the list.\n", search);
 
-  return 0;
+    // Binary search algorithm
+    while (first <= last) {
+        // If the middle element is less than the search value, ignore the left half
+        if (array[middle] < search)
+            first = middle + 1;
+        // If the middle element is the search value, print the result and exit
+        else if (array[middle] == search) {
+            printf("%d found at location %d.\n", search, middle + 1);
+            break;
+        }
+        // If the middle element is greater than the search value, ignore the right half
+        else
+            last = middle - 1;
+
+        // Update the middle index
+        middle = (first + last) / 2;
+    }
+    // If the search value is not found in the array, print a message
+    if (first > last)
+        printf("Not found! %d isn't present in the list.\n", search);
+
+    return 0;
 }
