8bab9b69dddb7bd32f04a908f36abb691a339203
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-07-16 22:43:36 -0700

    Update existence_triangle.cs

--- CSharp/existence_triangle.cs
+++ CSharp/existence_triangle.cs
@@ -1,31 +1,38 @@
-// the code calculates possible: to make a triangle for entered data
 using System;
 
 namespace Softcode
 {
+    /// <summary>
+    /// This program checks if it's possible to form a triangle with the given side lengths.
+    /// </summary>
     class MainClass
     {
+        /// <summary>
+        /// Main method to execute the program.
+        /// </summary>
         public static void Main(string[] args)
         {
+            // Prompt the user to enter the first side of the triangle
             Console.WriteLine("Enter the first side of the triangle: ");
             int a = Convert.ToInt32(Console.ReadLine());
 
+            // Prompt the user to enter the second side of the triangle
             Console.WriteLine("Enter the second side of the triangle: ");
             int b = Convert.ToInt32(Console.ReadLine());
 
+            // Prompt the user to enter the third side of the triangle
             Console.WriteLine("Enter the third side of the triangle: ");
             int c = Convert.ToInt32(Console.ReadLine());
 
-
-            if ((a < (b + c)) && (b < (a + c)) && (c < (a + b))) // basic conditions for a triangle
+            // Check if the given sides can form a triangle
+            if ((a < (b + c)) && (b < (a + c)) && (c < (a + b))) // Basic conditions for a triangle
             {
-                Console.WriteLine("YES");
+                Console.WriteLine("YES, a triangle can be formed.");
             }
             else
             {
-                Console.WriteLine("NO");
+                Console.WriteLine("NO, a triangle cannot be formed with the given side lengths.");
             }
         }
     }
 }
-
