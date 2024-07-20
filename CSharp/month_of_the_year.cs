a3ccf7485b7d0b1fff4ba5b39f9885743ecc3a11
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-07-19 20:54:50 -0700

    The provided C# program allows users to input a number representing a month of the year

--- CSharp/month_of_the_year.cs
+++ CSharp/month_of_the_year.cs
@@ -1,25 +1,40 @@
-//the tiny code - switch_case (month of the year) 
 using System;
 
 namespace Softcode
 {
     class MainClass
     {
+        /// <summary>
+        /// This program takes a number representing a month and outputs the corresponding season.
+        /// </summary>
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter your favorite month (number): ");
-            int a = Convert.ToInt32(Console.ReadLine());
-            a = a / 3;
-            switch (a)
+            int monthNumber = Convert.ToInt32(Console.ReadLine());
+
+            // Divide the month number by 3 to group months into seasons
+            int season = monthNumber / 3;
+
+            // Use a switch statement to determine the season based on the grouped month
+            switch (season)
             {
-                case (0): Console.WriteLine("Winter"); break;
-                case (4): Console.WriteLine("Winter"); break;
-                case (1): Console.WriteLine("Spring"); break;
-                case (2): Console.WriteLine("Summer"); break;
-                case (3):
-                    Console.WriteLine("Fall"); break;
+                case 0:
+                case 4:
+                    Console.WriteLine("Winter");
+                    break;
+                case 1:
+                    Console.WriteLine("Spring");
+                    break;
+                case 2:
+                    Console.WriteLine("Summer");
+                    break;
+                case 3:
+                    Console.WriteLine("Fall");
+                    break;
+                default:
+                    Console.WriteLine("Invalid month number entered.");
+                    break;
             }
         }
     }
 }
-
