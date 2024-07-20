4429bd5cf8e10ad5f5fdc9dfd32619008c8f069b
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-07-19 20:53:16 -0700

    The provided C# program is designed to determine if a lottery ticket number is considered lucky by comparing the sum of the first half of the number with the sum of the second half

--- CSharp/lucky_ticket.cs
+++ CSharp/lucky_ticket.cs
@@ -4,38 +4,33 @@ namespace Prime
 {
     class MainClass
     {
+        /// <summary>
+        /// This program checks if a lottery ticket number is lucky by comparing the sum of the first half and the second half of the number.
+        /// </summary>
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter your lottery ticket number: ");
             int six_digits_number = Convert.ToInt32(Console.ReadLine());
 
-            //we split the lottery ticket number into equal halves of 3 characters (numbers)
+            // Split the lottery ticket number into equal halves of 3 characters (numbers)
             int first_half = (six_digits_number / 1000);
             int second_half = (six_digits_number % 1000);
 
-            // calculate the sum of the numbers in the first half 
+            // Calculate the sum of the numbers in the first half 
             int s1 = first_half / 100;
-
             int x1 = (first_half % 100);
-
             int d1 = x1 / 10;
-
             int e1 = x1 % 10;
-
             int sum_first_half = (s1 + d1 + e1);
 
-            // calculate the sum of the second half
+            // Calculate the sum of the second half
             int s2 = second_half / 100;
-
             int x2 = (second_half % 100);
-
             int d2 = x2 / 10;
-
             int e2 = x2 % 10;
-
             int sum_second_half = (s2 + d2 + e2);
 
-            // compare the amounts of equal halves of the lottery ticket
+            // Compare the sums of equal halves of the lottery ticket
             if (sum_first_half == sum_second_half)
             {
                 Console.WriteLine("You have a lucky ticket!");
@@ -44,7 +39,6 @@ namespace Prime
             {
                 Console.WriteLine("Sorry! But try again - don't worry!");
             }
-
         }
     }
 }
