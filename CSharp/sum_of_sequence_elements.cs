8305c14dfaa49636e59390bf960510f316a2bf46
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-07-22 22:09:32 -0700

    This C# program calculates the sum of a sequence of integer numbers entered by the user, where the numbers are multiples of 2 but not multiples of 3

--- CSharp/sum_of_sequence_elements.cs
+++ CSharp/sum_of_sequence_elements.cs
@@ -1,30 +1,35 @@
-// tiny code - defines the sum of a sequence: 
-// in which the numbers are multiples of 2, but not multiples of 3
-
 using System;
 
 namespace Softcode
 {
+    /// <summary>
+    /// This program calculates the sum of a sequence of numbers that are multiples of 2 but not multiples of 3.
+    /// </summary>
     class MainClass
     {
+        /// <summary>
+        /// Main method where the program execution starts.
+        /// </summary>
+        /// <param name="args">Command-line arguments</param>
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter integer number: ");
+            Console.WriteLine("Enter integer numbers (enter 0 to finish): ");
             int n = Convert.ToInt32(Console.ReadLine());
 
             int sum = 0;
 
-            while (n != 0) // for terminate the process (enters numbers) - enter zero "0"
+            // Loop to calculate the sum of numbers meeting the condition
+            while (n != 0) // Entering 0 terminates the input process
             {
                 if (n % 2 == 0 && n % 3 != 0)
-                    sum = sum + n;
+                    sum += n;
+                
                 n = Convert.ToInt32(Console.ReadLine());
             }
+
             Console.WriteLine();
-            Console.Write("The sum of entered numbers, that meet the condition is: ");
+            Console.Write("The sum of entered numbers that meet the condition is: ");
             Console.WriteLine(sum);
         }
     }
 }
-
-
