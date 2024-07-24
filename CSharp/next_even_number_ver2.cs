ed0b74a7f1686d812556238dea38b70b86b0f7d9
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-07-23 21:54:46 -0700

    This C# program prompts the user to enter a number, calculates the next even number based on the input, and then displays the result.

--- CSharp/next_even_number_ver2.cs
+++ CSharp/next_even_number_ver2.cs
@@ -1,15 +1,24 @@
-// the tiny code represent - the next even number of entered number
 using System;
 
 namespace Softcode
 {
+    /// <summary>
+    /// This program calculates the next even number of the entered number.
+    /// </summary>
     class MainClass
     {
+        /// <summary>
+        /// Main method to get user input and calculate the next even number.
+        /// </summary>
         public static void Main(string[] args)
         {
+            // Prompt the user to enter a number
             Console.WriteLine("Enter your number: ");
 
+            // Read the input number from the user
             int n = Convert.ToInt32(Console.ReadLine());
+
+            // Calculate and display the next even number
             Console.Write(n + 2 - n % 2);
         }
     }
