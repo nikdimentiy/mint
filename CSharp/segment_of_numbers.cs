31547c9b55a986b5fda5bc9e02c2654ed574ae00
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-07-24 22:36:29 -0700

    This C# program calculates the product of numbers within a specified segment, from a user-defined starting integer 'a' to an ending integer 'b', inclusive, where the numbers end with the digit 7.

--- CSharp/segment_of_numbers.cs
+++ CSharp/segment_of_numbers.cs
@@ -1,28 +1,40 @@
-// the code calculate: product of numbers on the segment from a to b, ending in 7
 using System;
 
 namespace Softcode
 {
+    /// <summary>
+    /// This program calculates the product of numbers on the segment from a to b, ending in 7.
+    /// </summary>
     class MainClass
     {
+        /// <summary>
+        /// Main method where the program execution starts.
+        /// </summary>
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter a first integer number: ");
+            // Prompt the user to enter the first integer number.
+            Console.WriteLine("Enter the first integer number: ");
             int a = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter a second integer number: ");
+            // Prompt the user to enter the second integer number.
+            Console.WriteLine("Enter the second integer number: ");
             int b = Convert.ToInt32(Console.ReadLine());
 
+            // Initialize the product of the segment to 1.
             int productSegment = 1;
+
+            // Loop through the numbers from a to b and calculate the product of numbers ending in 7.
             for (int i = a; i <= b; i++)
             {
                 if (i % 10 == 7)
                 {
-                    productSegment = productSegment * i;
+                    productSegment *= i;
                 }
             }
+
+            // Display the result to the user.
             Console.WriteLine("The product of numbers on the segment from " 
-                          + a + " to " + b + " ending in 7 is: " + productSegment);
+                             + a + " to " + b + " ending in 7 is: " + productSegment);
         }
     }
 }
