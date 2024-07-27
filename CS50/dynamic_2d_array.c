0e604c93da572b1e40fb9da00b5f4f54ac78a450
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-07-26 22:28:55 -0700

    This C code demonstrates the dynamic allocation of a 2D array, initialization of its elements, and printing the array.

--- CS50/dynamic_2d_array.c
+++ CS50/dynamic_2d_array.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Function to print a 2D dynamic array
 void dynamic_array_print(int **A, size_t N, size_t M)
 {
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < M; j++)
         {
+            // Printing each element with a width of 5 characters
             printf("%*d", 5, A[i][j]);
         }
         printf("\n");
     }
 }
 
-/*
-    return pointer on 2d dynamic array
-    !allocates memory -> to be freed later
-*/
+// Function to allocate memory for a 2D dynamic array
 int **dynamic_array_alloc(size_t N, size_t M)
 {
     int **A = (int **)malloc(N * sizeof(int *));
@@ -27,6 +26,7 @@ int **dynamic_array_alloc(size_t N, size_t M)
     return A;
 }
 
+// Function to free memory allocated for a 2D dynamic array
 void dynamic_array_free(int **A, size_t N)
 {
     for (int i = 0; i < N; i++)
@@ -36,6 +36,7 @@ void dynamic_array_free(int **A, size_t N)
     free(A);
 }
 
+// Function to test dynamic array allocation, initialization, and printing
 void dynamic_array_test(size_t N, size_t M)
 {
     int **A = dynamic_array_alloc(N, M);
@@ -49,13 +50,15 @@ void dynamic_array_test(size_t N, size_t M)
         }
     }
     dynamic_array_print(A, N, M);
-    /*memory investigation*/
+
+    // Memory investigation
     printf("\n Pointers to lines: ");
-    for (int **p = A; p < A + 3; p++)
+    for (int **p = A; p < A + N; p++)
         printf("%10d", (long int)*p);
     printf("\n Direct memory access:\n");
-    for (int *p = (int *)*A; p < (int *)*A + 25; p++)
+    for (int *p = (int *)*A; p < (int *)*A + N * M; p++)
         printf("%d\t", *p);
+
     dynamic_array_free(A, N);
 }
 
