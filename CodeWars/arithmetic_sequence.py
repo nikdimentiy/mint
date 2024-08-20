b90531182ead66f776968da6fb41d42cd1cd63eb
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-08-19 18:21:28 -0700

    This code defines a function arithmetic_sequence_elements that generates the first n elements of an arithmetic progression based on a specified first element a and a common difference d.

--- CodeWars/arithmetic_sequence.py
+++ CodeWars/arithmetic_sequence.py
@@ -1,17 +1,27 @@
-# In your class, you have started lessons about arithmetic progression. Since you are also a programmer,
-# you have decided to write a function that will return the first n elements of the sequence
-# with the given common difference d and first element a. Note that the difference may be zero!
-
-# The result should be a string of numbers, separated by comma and space.
-
-
 def arithmetic_sequence_elements(a, d, n):
-    # generate sequence using list comprehension
+    """
+    Generate the first n elements of an arithmetic progression.
+
+    Parameters:
+    a (int): The first element of the sequence.
+    d (int): The common difference between consecutive elements.
+    n (int): The number of elements to generate.
+
+    Returns:
+    str: A string representation of the first n elements of the arithmetic sequence,
+         separated by a comma and a space.
+    """
+    # Generate the sequence using list comprehension
     sequence = [a + (i * d) for i in range(n)]
-    # join sequence elements into a string separated by comma and space
+    
+    # Join the sequence elements into a string separated by comma and space
     return ", ".join(str(i) for i in sequence)
 
-
-print(arithmetic_sequence_elements(1, 2, 5))  # output: "1, 3, 5, 7, 9"
-print(arithmetic_sequence_elements(0, 0, 5))  # output: "0, 0, 0, 0, 0"
-print(arithmetic_sequence_elements(3, -1, 5))  # output: "3, 2, 1, 0, -1"
+# Driver code to test the function
+if __name__ == "__main__":
+    # Test cases
+    print(arithmetic_sequence_elements(1, 2, 5))  # Expected output: "1, 3, 5, 7, 9"
+    print(arithmetic_sequence_elements(0, 0, 5))  # Expected output: "0, 0, 0, 0, 0"
+    print(arithmetic_sequence_elements(3, -1, 5))  # Expected output: "3, 2, 1, 0, -1"
+    print(arithmetic_sequence_elements(5, 5, 5))  # Expected output: "5, 10, 15, 20, 25"
+    print(arithmetic_sequence_elements(10, -2, 4))  # Expected output: "10, 8, 6, 4"
