921caf4fd25bb10f0d250aafb329b79325da53fa
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-08-26 22:29:52 -0700

    This code defines a Ghost class that represents a ghost with a randomly assigned color. The colors can be one of four options: "white," "yellow," "purple," or "red." When an instance of the Ghost class is created, its color is randomly selected from this list.

--- CodeWars/color_ghost.py
+++ CodeWars/color_ghost.py
@@ -1,7 +1,33 @@
-# CodeWars task: solution --> https://www.codewars.com/kata/53f1015fa9fe02cbda00111a/python
-
 import random
 
 class Ghost(object):
-  def __init__(self):
-    self.color = random.choice(["white", "yellow", "purple", "red"])
+    """
+    A class representing a ghost with a randomly assigned color.
+
+    Attributes:
+        color (str): The color of the ghost, which can be one of the following:
+                     "white", "yellow", "purple", or "red".
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the Ghost class.
+        
+        The color of the ghost is randomly selected from the predefined list
+        of colors when an instance is created.
+        """
+        self.color = random.choice(["white", "yellow", "purple", "red"])
+
+# Driver code to demonstrate the functionality of the Ghost class
+if __name__ == "__main__":
+    # Create a list to hold multiple ghost instances
+    ghosts = []
+
+    # Create 5 ghost instances and store them in the list
+    for _ in range(5):
+        ghost = Ghost()
+        ghosts.append(ghost)
+
+    # Print the colors of the created ghosts
+    for i, ghost in enumerate(ghosts):
+        print(f"Ghost {i + 1} color: {ghost.color}")
