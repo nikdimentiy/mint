8e27e69b0644610c218182aa554bb31c61b91431
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-09-07 22:34:27 -0700

    This code defines a function colour_association that converts a 2D array of colors and their common associations into a list of dictionaries. Each dictionary maps a color (as the key) to its corresponding association (as the value).

--- CodeWars/color_association.py
+++ CodeWars/color_association.py
@@ -1,9 +1,31 @@
-# Colour plays an important role in our lifes. Most of us like this colour better then another.
-# User experience specialists believe that certain colours have certain psychological meanings for us.
-
-# You are given a 2D array, composed of a colour and its 'common' association in each array element.
-# The function you will write needs to return the colour as 'key' and association as its 'value'.
-
-
 def colour_association(arr):
+    """
+    This function takes a 2D array where each element contains a color and its common association.
+    It returns a list of dictionaries, where each dictionary has the color as the key and its 
+    association as the value.
+
+    Parameters:
+    arr (list of list): A 2D array containing colors and their associations.
+
+    Returns:
+    list of dict: A list of dictionaries with colors as keys and associations as values.
+    """
+    # Create a list of dictionaries from the input array
     return [{i[0]: i[1]} for i in arr]
+
+# Driver code to test the function
+if __name__ == "__main__":
+    # Sample input: a 2D array of colors and their associations
+    color_associations = [
+        ["Red", "Passion"],
+        ["Blue", "Calm"],
+        ["Green", "Nature"],
+        ["Yellow", "Happiness"],
+        ["Black", "Mystery"]
+    ]
+
+    # Call the function and store the result
+    result = colour_association(color_associations)
+
+    # Print the result
+    print(result)
