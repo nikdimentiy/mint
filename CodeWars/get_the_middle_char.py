f39c475b13162b060bfea72eecb4f8c03855ba77
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-12-05 19:25:37 -0800

    This code defines a function get_middle that extracts and returns the middle character(s) of a given string. If the string's length is odd, it returns the single middle character; if the length is even, it returns the two middle characters.

--- CodeWars/get_the_middle_char.py
+++ CodeWars/get_the_middle_char.py
@@ -1,24 +1,33 @@
-# You are going to be given a word. Your job is to return the middle character of the word.
-# If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
-
-# Examples
-
-# Kata.getMiddle("test") should return "es"
-
-# Kata.getMiddle("testing") should return "t"
-
-# Kata.getMiddle("middle") should return "dd"
-
-# Kata.getMiddle("A") should return "A"
-# Input
-
-# A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error
-# in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
-
-# Output
-
-# The middle character(s) of the word represented as a string.
-
-
 def get_middle(s):
-    return s[len(s)//2] if len(s) % 2 != 0 else s[len(s)//2-1:len(s)//2+1]
+    """
+    Returns the middle character(s) of a given word.
+    
+    If the length of the word is odd, it returns the middle character.
+    If the length of the word is even, it returns the two middle characters.
+    
+    Parameters:
+    s (str): A word (string) of length 0 < str < 1000.
+    
+    Returns:
+    str: The middle character(s) of the word.
+    """
+    # Calculate the length of the string
+    length = len(s)
+    
+    # Check if the length is odd
+    if length % 2 != 0:
+        # Return the middle character
+        return s[length // 2]
+    else:
+        # Return the two middle characters
+        return s[length // 2 - 1:length // 2 + 1]
+
+# Driver code to test the function
+if __name__ == "__main__":
+    # Test cases
+    print(get_middle("test"))      # Should return "es"
+    print(get_middle("testing"))   # Should return "t"
+    print(get_middle("middle"))    # Should return "dd"
+    print(get_middle("A"))         # Should return "A"
+    print(get_middle("example"))   # Should return "am"
+    print(get_middle("abcdef"))    # Should return "cd"
